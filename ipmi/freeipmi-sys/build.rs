af25c7ce703cc66a1b2b081969fe026540b5027e
Vishnu Rangayyan <vrangayyan@nvidia.com>
2022-05-10 12:24:15 -0500

    Implement a rust ffi crate for libfreeipmi Add systest utility to use freeipmi-sys lib Add vendored crates needed for freeipmi-sys

---
+++ ipmi/freeipmi-sys/build.rs
@@ -0,0 +1,12 @@
+use std::env;
+/// check and make sure libfreeipmi and libfreeipmi headers are installed
+fn main() {
+    let statik = env::var("CARGO_FEATURE_STATIC").is_ok();
+
+    let _libfreeipmi = pkg_config::Config::new()
+        .cargo_metadata(false)
+        .statik(statik)
+        .probe("libfreeipmi")
+        .unwrap();
+
+}
