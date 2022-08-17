59758b6807ea42514d240df7f2e2034038ad008e
Michael Basnight <mbasnight@nvidia.com>
2022-08-17 16:08:04 -0500

    Fix vendor string sql

--- api/migrations/20220815220815_add_dhcp.sql
+++ api/migrations/20220815220815_add_dhcp.sql
@@ -1,7 +1,7 @@
 CREATE TABLE dhcp_entries
 (
     machine_interface_id uuid NOT NULL,
-    vendor_string VARCHAR NOT NULL UNIQUE,
+    vendor_string VARCHAR NOT NULL,
 
     PRIMARY KEY(machine_interface_id, vendor_string),
     FOREIGN KEY(machine_interface_id) REFERENCES machine_interfaces(id)
