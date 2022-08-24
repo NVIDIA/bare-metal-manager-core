80c4988151e83f411355298b92487b3f2c0058b4
Ron Thompson <ront@nvidia.com>
2022-08-24 14:52:34 -0700

    fix rustc compiler warnings and linter errors in pxe

--- pxe/src/routes/ipxe.rs
+++ pxe/src/routes/ipxe.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 
-use rocket::Route;
+use rocket::{get, routes, Route};
 use rocket_dyn_templates::Template;
+
 use rpc::forge::v0 as rpc;
 
 use crate::{Machine, RuntimeConfig};
@@ -28,7 +29,7 @@ boot ||
 "#,
             b.kernel, b.command_line, b.initrd
         );
-        println!("Output of the generation - {:?}", output.to_string());
+        println!("Output of the generation - {:?}", output);
         output
     }
 }
@@ -61,15 +62,15 @@ fn determine_boot_from_state(
         "new" => "exit 1".to_string(),
         "assigned" => boot_into_netbootxyz(),
         // any unrecognized state will cause ipxe to stop working with this message
-        s => format!(
+        invalid_status => format!(
             r#"
 echo could not continue boot due to invalid status - {} ||
 sleep 5 ||
 exit ||
 "#,
-            s
+            invalid_status
         )
-        .to_string(),
+        ,
     }
 }
 
@@ -80,23 +81,27 @@ chain --autofree https://boot.netboot.xyz
     .to_string()
 }
 
-fn boot_into_discovery(arch: rpc::MachineArchitecture, interface: rpc::MachineInterface, config: RuntimeConfig) -> String {
+fn boot_into_discovery(
+    arch: rpc::MachineArchitecture,
+    interface: rpc::MachineInterface,
+    config: RuntimeConfig,
+) -> String {
     let uuid = interface.id.unwrap();
     match arch {
         rpc::MachineArchitecture::Arm => {
             String::from(BootInstructionGenerator {
-                kernel: format!("{pxe_url}/public/blobs/internal/aarch64/carbide.efi", pxe_url=config.pxe_url),
-                initrd: format!("{pxe_url}/public/blobs/internal/aarch64/carbide.root", pxe_url=config.pxe_url),
-                command_line: format!("root=live:{pxe_url}/public/blobs/internal/x86_64/carbide.root console=tty0 console=ttyS0 console=ttyAMA0 console=hvc0 ip=dhcp cli_cmd=discovery machine_id={uuid} bfnet=oob_net0:dhcp bfks={pxe_url}/api/v0/cloud-init/{uuid}/user-data pxe_uri={pxe_url} server_uri={api_url} ", pxe_url=config.pxe_url, uuid=uuid, api_url=config.api_url),
+                kernel: format!("{pxe_url}/public/blobs/internal/aarch64/carbide.efi", pxe_url = config.pxe_url),
+                initrd: format!("{pxe_url}/public/blobs/internal/aarch64/carbide.root", pxe_url = config.pxe_url),
+                command_line: format!("root=live:{pxe_url}/public/blobs/internal/x86_64/carbide.root console=tty0 console=ttyS0 console=ttyAMA0 console=hvc0 ip=dhcp cli_cmd=discovery machine_id={uuid} bfnet=oob_net0:dhcp bfks={pxe_url}/api/v0/cloud-init/{uuid}/user-data pxe_uri={pxe_url} server_uri={api_url} ", pxe_url = config.pxe_url, uuid = uuid, api_url = config.api_url),
             })
-        },
+        }
         rpc::MachineArchitecture::X86 => {
             String::from(BootInstructionGenerator {
-                kernel: format!("{pxe_url}/public/blobs/internal/x86_64/carbide.efi", pxe_url=config.pxe_url),
-                initrd: format!("{pxe_url}/public/blobs/internal/x86_64/carbide.root", pxe_url=config.pxe_url),
-                command_line: format!("root=live:{pxe_url}/public/blobs/internal/x86_64/carbide.root console=tty0 console=ttyS0 ip=dhcp cli_cmd=discovery machine_id={uuid} bfnet=oob_net0:dhcp bfks={pxe_url}/api/v0/cloud-init/{uuid}/user-data pxe_uri={pxe_url} server_uri={api_url} ", pxe_url=config.pxe_url, uuid=uuid, api_url=config.api_url),
+                kernel: format!("{pxe_url}/public/blobs/internal/x86_64/carbide.efi", pxe_url = config.pxe_url),
+                initrd: format!("{pxe_url}/public/blobs/internal/x86_64/carbide.root", pxe_url = config.pxe_url),
+                command_line: format!("root=live:{pxe_url}/public/blobs/internal/x86_64/carbide.root console=tty0 console=ttyS0 ip=dhcp cli_cmd=discovery machine_id={uuid} bfnet=oob_net0:dhcp bfks={pxe_url}/api/v0/cloud-init/{uuid}/user-data pxe_uri={pxe_url} server_uri={api_url} ", pxe_url = config.pxe_url, uuid = uuid, api_url = config.api_url),
             })
-        },
+        }
     }
 }
 
