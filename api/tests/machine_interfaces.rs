64c578ddda3cd91b959de3aef91799278b7760bc
Andrew Forgue <aforgue@nvidia.com>
2022-09-21 17:28:13 +0000

    tests: update machine_interfaces test

--- api/tests/machine_interfaces.rs
+++ api/tests/machine_interfaces.rs
@@ -1,18 +1,15 @@
-use std::str::FromStr;
+use carbide::db::network_segment::NetworkSegment;
 use log::LevelFilter;
 use mac_address::MacAddress;
+use sqlx::{Connection, Postgres};
+use std::str::FromStr;
 
 use carbide::db::address_selection_strategy::AddressSelectionStrategy;
 use carbide::db::machine::Machine;
 use carbide::db::machine_interface::MachineInterface;
-use carbide::db::network_prefix::NewNetworkPrefix;
-use carbide::db::network_segment::{NetworkSegment, NewNetworkSegment};
-use carbide::db::vpc::NewVpc;
 use carbide::CarbideError;
 
-use crate::common::TestDatabaseManager;
-
-mod common;
+const FIXTURE_NETWORK_SEGMENT_ID: uuid::Uuid = uuid::uuid!("91609f10-c91d-470d-a260-6293ea0c1200");
 
 #[ctor::ctor]
 fn setup() {
@@ -21,152 +18,84 @@ fn setup() {
         .init();
 }
 
-#[tokio::test]
-async fn only_one_primary_interface_per_machine() {
-    let pool = TestDatabaseManager::new()
-        .await
-        .expect("Unable to create database pool")
-        .pool;
+async fn get_fixture_network_segment(
+    txn: &mut sqlx::Transaction<'_, Postgres>,
+) -> Result<NetworkSegment, Box<dyn std::error::Error>> {
+    carbide::db::network_segment::NetworkSegment::find(
+        txn,
+        carbide::db::UuidKeyedObjectFilter::One(FIXTURE_NETWORK_SEGMENT_ID),
+    )
+    .await?
+    .pop()
+    .ok_or_else(|| {
+        format!(
+            "Can't find the Network Segment by well-known-uuid: {}",
+            FIXTURE_NETWORK_SEGMENT_ID
+        )
+        .into()
+    })
+}
 
-    let mut txn = pool.begin().await.expect("Unable to create txn");
+#[sqlx::test(fixtures("create_domain", "create_vpc", "create_network_segment"))]
+async fn only_one_primary_interface_per_machine(
+    pool: sqlx::PgPool,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut txn = pool.begin().await?;
 
-    let mut txn2 = pool
-        .begin()
-        .await
-        .expect("Unable to create transaction on database pool");
-
-    let mut txn3 = pool
-        .begin()
-        .await
-        .expect("Unable to create transaction on database pool");
-
-    let vpc = NewVpc {
-        name: "Test VPC".to_string(),
-        organization: String::new(),
-    }
-    .persist(&mut txn)
-    .await
-    .expect("Unable to create VPC");
-
-    let new_segment: NetworkSegment = NewNetworkSegment {
-        name: "test-network".to_string(),
-        subdomain_id: None,
-        mtu: 1500i32,
-        vpc_id: Some(vpc.id),
-
-        prefixes: vec![
-            NewNetworkPrefix {
-                prefix: "2001:db8:f::/64".parse().unwrap(),
-                gateway: None,
-                num_reserved: 100,
-            },
-            NewNetworkPrefix {
-                prefix: "192.0.2.0/24".parse().unwrap(),
-                gateway: "192.0.2.1".parse().ok(),
-                num_reserved: 2,
-            },
-        ],
-    }
-    .persist(&mut txn)
-    .await
-    .expect("Unable to create network segment");
-
-    txn.commit().await.unwrap();
+    let network_segment = get_fixture_network_segment(&mut txn.begin().await?).await?;
 
     let new_interface = MachineInterface::create(
-        &mut txn2,
-        &new_segment,
+        &mut txn,
+        &network_segment,
         MacAddress::from_str("ff:ff:ff:ff:ff:ff").as_ref().unwrap(),
         None,
         "peppersmacker2".to_string(),
         true,
         AddressSelectionStrategy::Automatic,
     )
-    .await
-    .expect("Unable to create machine interface");
+    .await?;
 
-    let new_machine = Machine::create(&mut txn2, new_interface)
+    let new_machine = Machine::create(&mut txn, new_interface)
         .await
         .expect("Unable to create machine");
 
-    txn2.commit().await.unwrap();
+    txn.commit().await.unwrap();
+
+    let mut txn = pool.begin().await?;
 
     let mut should_failed_machine_interface = MachineInterface::create(
-        &mut txn3,
-        &new_segment,
+        &mut txn,
+        &network_segment,
         MacAddress::from_str("ff:ff:ff:ff:ff:ef").as_ref().unwrap(),
         None,
         "peppersmacker2".to_string(),
         true,
         AddressSelectionStrategy::Automatic,
     )
-    .await
-    .expect("Unable to create second interface");
+    .await?;
 
     let output = should_failed_machine_interface
-        .associate_interface_with_machine(&mut txn3, new_machine.id())
+        .associate_interface_with_machine(&mut txn, new_machine.id())
         .await;
-
-    txn3.commit().await.unwrap();
-
-    assert!(matches!(output, Err(CarbideError::OnePrimaryInterface)));
-}
-
-#[tokio::test]
-async fn many_non_primary_interfaces_per_machine() {
-    let pool = TestDatabaseManager::new()
-        .await
-        .expect("Unable to create database pool")
-        .pool;
-
-    let mut txn = pool.begin().await.expect("Unable to create txn");
-
-    let mut txn2 = pool
-        .begin()
-        .await
-        .expect("Unable to create transaction on database pool");
-
-    let mut txn3 = pool
-        .begin()
-        .await
-        .expect("Unable to create transaction on database pool");
-
-    let vpc = NewVpc {
-        name: "Test VPC".to_string(),
-        organization: String::new(),
-    }
-    .persist(&mut txn)
-    .await
-    .expect("Unable to create VPC");
-
-    let new_segment: NetworkSegment = NewNetworkSegment {
-        name: "test-network".to_string(),
-        subdomain_id: None,
-        mtu: 1500i32,
-        vpc_id: Some(vpc.id),
-
-        prefixes: vec![
-            NewNetworkPrefix {
-                prefix: "2001:db8:f::/64".parse().unwrap(),
-                gateway: None,
-                num_reserved: 100,
-            },
-            NewNetworkPrefix {
-                prefix: "192.0.2.0/24".parse().unwrap(),
-                gateway: "192.0.2.1".parse().ok(),
-                num_reserved: 2,
-            },
-        ],
-    }
-    .persist(&mut txn)
-    .await
-    .expect("Unable to create network segment");
 
     txn.commit().await.unwrap();
 
+    assert!(matches!(output, Err(CarbideError::OnePrimaryInterface)));
+
+    Ok(())
+}
+
+#[sqlx::test(fixtures("create_domain", "create_vpc", "create_network_segment"))]
+async fn many_non_primary_interfaces_per_machine(
+    pool: sqlx::PgPool,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut txn = pool.begin().await?;
+
+    let network_segment = get_fixture_network_segment(&mut txn.begin().await?).await?;
+
     MachineInterface::create(
-        &mut txn2,
-        &new_segment,
+        &mut txn,
+        &network_segment,
         MacAddress::from_str("ff:ff:ff:ff:ff:ff").as_ref().unwrap(),
         None,
         "peppersmacker2".to_string(),
@@ -176,11 +105,12 @@ async fn many_non_primary_interfaces_per_machine() {
     .await
     .expect("Unable to create machine interface");
 
-    txn2.commit().await.unwrap();
+    txn.commit().await.unwrap();
+    let mut txn = pool.begin().await?;
 
     let should_be_ok_interface = MachineInterface::create(
-        &mut txn3,
-        &new_segment,
+        &mut txn,
+        &network_segment,
         MacAddress::from_str("ff:ff:ff:ff:ff:ef").as_ref().unwrap(),
         None,
         "peppersmacker2".to_string(),
@@ -189,49 +119,9 @@ async fn many_non_primary_interfaces_per_machine() {
     )
     .await;
 
-    txn3.commit().await.unwrap();
+    txn.commit().await.unwrap();
 
     assert!(should_be_ok_interface.is_ok());
-}
 
-#[tokio::test]
-async fn valdate_mac_address_before_creating_interface() {
-    let pool = TestDatabaseManager::new()
-        .await
-        .expect("Unable to create database pool")
-        .pool;
-
-    let mut txn = pool.begin().await.expect("Unable to create txn");
-    let vpc = NewVpc {
-        name: "Test VPC".to_string(),
-        organization: String::new(),
-    }
-    .persist(&mut txn)
-    .await
-    .expect("Unable to create VPC");
-
-    NewNetworkSegment {
-        name: "test-network".to_string(),
-        subdomain_id: None,
-        mtu: 1500i32,
-        vpc_id: Some(vpc.id),
-
-        prefixes: vec![
-            NewNetworkPrefix {
-                prefix: "2001:db8:f::/64".parse().unwrap(),
-                gateway: None,
-                num_reserved: 100,
-            },
-            NewNetworkPrefix {
-                prefix: "192.0.2.0/24".parse().unwrap(),
-                gateway: "192.0.2.1".parse().ok(),
-                num_reserved: 2,
-            },
-        ],
-    }
-    .persist(&mut txn)
-    .await
-    .expect("Unable to create network segment");
-
-    txn.commit().await.unwrap();
+    Ok(())
 }
