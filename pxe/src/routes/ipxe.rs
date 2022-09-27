1a093515a02f4f380364974c9bb61aa4f661cbba
Abhishek Varshney <abvarshney@nvidia.com>
2022-09-27 08:18:50 -0700

    Ipxe changes for instance management.

--- pxe/src/routes/ipxe.rs
+++ pxe/src/routes/ipxe.rs
@@ -1,11 +1,11 @@
 use std::collections::HashMap;
 
+use ::rpc::forge::v0 as rpc;
 use rocket::{get, routes, Route};
 use rocket_dyn_templates::Template;
 
-use rpc::forge::v0 as rpc;
-
 use crate::machine_architecture::MachineArchitecture;
+use crate::routes::RpcContext;
 use crate::{Machine, RuntimeConfig};
 
 #[derive(serde::Serialize)]
@@ -72,7 +72,7 @@ pub async fn whoami(machine: Machine) -> Template {
 pub async fn boot(contents: Machine, config: RuntimeConfig) -> Template {
     let instructions = match contents.machine {
         None => boot_into_discovery(contents.architecture, contents.interface, config),
-        Some(m) => determine_boot_from_state(m, contents.interface, config),
+        Some(m) => determine_boot_from_state(m, contents.interface, config).await,
     };
 
     let mut context = HashMap::new();
@@ -81,7 +81,7 @@ pub async fn boot(contents: Machine, config: RuntimeConfig) -> Template {
     Template::render("pxe", &context)
 }
 
-fn determine_boot_from_state(
+async fn determine_boot_from_state(
     machine: rpc::Machine,
     interface: rpc::MachineInterface,
     config: RuntimeConfig,
@@ -90,7 +90,7 @@ fn determine_boot_from_state(
         // The DPU needs an error code to force boot into the OS
         "ready" => "exit 1".to_string(),
         "reset" => boot_into_discovery(rpc::MachineArchitecture::X86, interface, config),
-        "assigned" => boot_into_netbootxyz(),
+        "assigned" => boot_tenant_config(machine, config).await,
         // any unrecognized state will cause ipxe to stop working with this message
         invalid_status => format!(
             r#"
@@ -103,11 +103,26 @@ exit ||
     }
 }
 
-fn boot_into_netbootxyz() -> String {
-    r#"
-chain --autofree https://boot.netboot.xyz
-"#
-    .to_string()
+async fn boot_tenant_config(machine: rpc::Machine, config: RuntimeConfig) -> String {
+    let machine_id = match machine.id {
+        Some(id) => id,
+        //TODO: Need a better way to deal with missing id.
+        None => return "Machine ID is invalid".to_string(),
+    };
+
+    match RpcContext::get_instance(machine_id, config.api_url.clone()).await {
+        Ok(instance) => instance.custom_ipxe,
+        Err(err) => {
+            eprintln!("{}", err);
+            format!(
+                r#"
+echo Failed to fetch custome_ipxe: {} || 
+exit 101 ||
+"#,
+                err
+            )
+        }
+    }
 }
 
 fn boot_into_discovery(
