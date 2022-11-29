b7351df42d664f3b7dbb4675e80dc24620c7f40c
Michael Basnight <mbasnight@nvidia.com>
2022-11-29 08:48:03 -0800

    Small changes before alpha demo

--- pxe/ipxe/local/branding.h
+++ pxe/ipxe/local/branding.h
@@ -1,5 +1,5 @@
 #undef PRODUCT_NAME
-#define PRODUCT_NAME "NVIDIA Forge"
+#define PRODUCT_NAME "Forge"
 
 #undef PRODUCT_SHORT_NAME
 #define PRODUCT_SHORT_NAME "forge"
@@ -8,4 +8,4 @@
 #define PRODUCT_URI "http://forge.nvidia.com"
 
 #undef PRODUCT_TAG_LINE
-#define PRODUCT_TAG_LINE "NVIDIA Forge Bare Metal provisioning"
+#define PRODUCT_TAG_LINE "Forge Bare Metal provisioning"
