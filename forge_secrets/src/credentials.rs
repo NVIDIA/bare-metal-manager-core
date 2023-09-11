ab7116c7545bf6b2d066ef68597cbe9a2da64e9b
Matthias Einwag <meinwag@nvidia.com>
2023-09-11 16:42:41 +0000

    feat: Change path for shared DPU credentials in site-vault
    and add the capability to store/retrieve shared Host credentials.
    
    This is not yet a breaking change, since the credentials
    haven't yet been stored on any site.
    Storing the host credentials will help us with implementing the
    redfish proxy (https://gitlab-master.nvidia.com/nvmetal/designs/-/merge_requests/13)

--- forge_secrets/src/credentials.rs
+++ forge_secrets/src/credentials.rs
@@ -39,6 +39,9 @@ pub enum CredentialKey {
     DpuRedfish {
         credential_type: CredentialType,
     },
+    HostRedfish {
+        credential_type: CredentialType,
+    },
 }
 
 impl CredentialKey {
@@ -58,10 +61,18 @@ impl CredentialKey {
             }
             CredentialKey::DpuRedfish { credential_type } => match credential_type {
                 CredentialType::HardwareDefault => {
-                    "machines/all_factory_default/bmc-metadata-items/root".to_string()
+                    "machines/all_dpus/factory_default/bmc-metadata-items/root".to_string()
                 }
                 CredentialType::SiteDefault => {
-                    "machines/all_site_default/bmc-metadata-items/root".to_string()
+                    "machines/all_dpus/site_default/bmc-metadata-items/root".to_string()
+                }
+            },
+            CredentialKey::HostRedfish { credential_type } => match credential_type {
+                CredentialType::HardwareDefault => {
+                    "machines/all_hosts/factory_default/bmc-metadata-items/root".to_string()
+                }
+                CredentialType::SiteDefault => {
+                    "machines/all_hosts/site_default/bmc-metadata-items/root".to_string()
                 }
             },
         }
