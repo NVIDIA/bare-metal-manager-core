f443ebb5b92c7fbad394f71c8165393cdd99be36
Graham King <grahamk@nvidia.com>
2023-09-13 17:46:49 -0400

    test: Move

--- api/tests/machine_states.rs
+++ api/tests/machine_states.rs
@@ -16,7 +16,7 @@ use std::sync::Arc;
 use carbide::db::machine::{Machine, MachineSearchConfig};
 use carbide::model::machine::{FailureDetails, ManagedHostState};
 use carbide::state_controller::machine::handler::MachineStateHandler;
-use common::api_fixtures::create_test_env;
+use common::api_fixtures::{create_managed_host, create_test_env};
 
 use crate::common::api_fixtures::run_state_controller_iteration;
 
@@ -85,4 +85,43 @@ async fn test_failed_state_host(pool: sqlx::PgPool) {
         host.current_state(),
         ManagedHostState::Failed { .. }
     ));
+}
+
+/// If the DPU stops sending us health updates we eventually mark it unhealthy
+#[sqlx::test(fixtures("create_domain", "create_vpc", "create_network_segment"))]
+async fn test_dpu_heartbeat(pool: sqlx::PgPool) -> sqlx::Result<()> {
+    let env = create_test_env(pool.clone()).await;
+    let (host_machine_id, dpu_machine_id) = create_managed_host(&env).await;
+    let mut txn = pool.begin().await.unwrap();
+
+    // create_dpu_machine runs record_dpu_network_status, so machine should be healthy
+    let dpu_machine = Machine::find_by_query(&mut txn, &dpu_machine_id.to_string())
+        .await
+        .unwrap()
+        .expect("expect DPU to be found");
+    assert!(matches!(dpu_machine.has_healthy_network(), Ok(true)));
+
+    // Tell state handler to mark DPU as unhealthy after 1 second
+    let handler = MachineStateHandler::new(chrono::Duration::seconds(1));
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+    // Run the state state handler
+    let services = Arc::new(env.state_handler_services());
+    run_state_controller_iteration(
+        &services,
+        &pool,
+        &env.machine_state_controller_io,
+        host_machine_id.clone(),
+        &handler,
+    )
+    .await;
+
+    // Now the network should be marked unhealthy
+    let dpu_machine = Machine::find_by_query(&mut txn, &dpu_machine_id.to_string())
+        .await
+        .unwrap()
+        .expect("expect DPU to be found");
+    assert!(matches!(dpu_machine.has_healthy_network(), Ok(false)));
+
+    Ok(())
 }
