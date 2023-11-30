1b38f4b67d980cb2c2ee2359a58be374f098d660
abvarshney <abvarshney@nvidia.com>
2023-11-30 12:42:41 +0530

    fix: Move to async cmd for DPU restart

--- utils/src/cmd.rs
+++ utils/src/cmd.rs
@@ -1,5 +1,7 @@
 use std::ffi::OsStr;
 use std::process::Command;
+use std::time::Duration;
+use tokio::process::Command as TokioCommand;
 
 #[derive(thiserror::Error, Debug)]
 pub enum CmdError {
@@ -31,11 +33,10 @@ impl CmdError {
             error_details,
         )
     }
-    pub fn output_parse_error(command: &Cmd) -> Self {
+    pub fn output_parse_error(command: &Command) -> Self {
         Self::OutputParse(
-            command.command.get_program().to_string_lossy().to_string(),
+            command.get_program().to_string_lossy().to_string(),
             command
-                .command
                 .get_args()
                 .map(|arg| arg.to_string_lossy().to_string())
                 .collect::<Vec<String>>(),
@@ -97,11 +98,80 @@ impl Cmd {
 
             if output.status.success() {
                 return String::from_utf8(output.stdout)
-                    .map_err(|_| CmdError::output_parse_error(&self));
+                    .map_err(|_| CmdError::output_parse_error(&self.command));
             }
+
+            // Give some breathing time.
+            std::thread::sleep(Duration::from_millis(100));
         }
         if let Some(output) = last_output {
             Err(CmdError::subprocess_error(&self.command, &output))
+        } else {
+            Err(CmdError::Generic("Invalid retry value".to_owned()))
+        }
+    }
+}
+
+/// Async implementation of Cmd.
+#[derive(Debug)]
+pub struct TokioCmd {
+    command: TokioCommand,
+    attempts: u32,
+}
+
+impl TokioCmd {
+    pub fn new<S: AsRef<OsStr>>(program: S) -> Self {
+        Self {
+            command: TokioCommand::new(program),
+            attempts: 1,
+        }
+    }
+
+    pub fn args<I, S>(mut self, args: I) -> Self
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        self.command.args(args);
+        self
+    }
+
+    pub fn env<S>(mut self, key: S, value: S) -> Self
+    where
+        S: AsRef<OsStr>,
+    {
+        self.command.env(key, value);
+        self
+    }
+
+    pub fn attempts(mut self, attempts: u32) -> Self {
+        self.attempts = attempts;
+        self
+    }
+
+    pub async fn output(mut self) -> CmdResult<String> {
+        if cfg!(test) {
+            return Ok("test string".to_string());
+        }
+
+        let mut last_output = None;
+        for _attempt in 0..self.attempts {
+            let output = self
+                .command
+                .output()
+                .await
+                .map_err(|x| CmdError::Generic(x.to_string()))?;
+
+            last_output = Some(output.clone());
+
+            if output.status.success() {
+                return String::from_utf8(output.stdout)
+                    .map_err(|_| CmdError::output_parse_error(self.command.as_std()));
+            }
+            tokio::time::sleep(Duration::from_millis(100)).await;
+        }
+        if let Some(output) = last_output {
+            Err(CmdError::subprocess_error(self.command.as_std(), &output))
         } else {
             Err(CmdError::Generic("Invalid retry value".to_owned()))
         }
