195544042353f93130d732815a429ed930077253
amaslennikov <amaslennikov@nvidia.com>
2024-01-09 20:19:09 +0300

    feat: Add FMDS IB API The API is based on the design doc
    Signed-off-by: amaslennikov <amaslennikov@nvidia.com>

--- bluefield/agent/src/instance_metadata_endpoint.rs
+++ bluefield/agent/src/instance_metadata_endpoint.rs
@@ -20,10 +20,28 @@ use crate::instance_metadata_fetcher::InstanceMetadataReader;
 const PUBLIC_IPV4_CATEGORY: &str = "public-ipv4";
 const HOSTNAME_CATEGORY: &str = "hostname";
 const USER_DATA_CATEGORY: &str = "user-data";
+const GUID: &str = "guid";
+const IB_PARTITION: &str = "partition";
+const LID: &str = "lid";
 
 pub fn get_instance_metadata_router(metadata_fetcher: Arc<dyn InstanceMetadataReader>) -> Router {
     // TODO add handling for non-supported URIs
+    let ib_router = Router::new()
+        .route("/devices", get(get_devices))
+        .route("/devices/:device", get(get_instances))
+        .nest(
+            "/devices/:device",
+            Router::new()
+                .route("/instances", get(get_instances))
+                .route("/instances/:instance", get(get_instance_attributes))
+                .route(
+                    "/instances/:instance/:attribute",
+                    get(get_instance_attribute),
+                ),
+        );
+
     Router::new()
+        .nest("/infiniband", ib_router)
         .route("/:category", get(get_metadata_parameter))
         .with_state(metadata_fetcher)
 }
@@ -50,11 +68,188 @@ async fn get_metadata_parameter(
     }
 }
 
+async fn get_devices(State(state): State<Arc<dyn InstanceMetadataReader>>) -> (StatusCode, String) {
+    let read_guard = state.read();
+    let metadata = match read_guard.as_ref() {
+        Some(metadata) => metadata,
+        None => {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "metadata currently unavailable".to_string(),
+            );
+        }
+    };
+
+    let mut response = String::new();
+    if let Some(devices) = &metadata.ib_devices {
+        for (index, device) in devices.iter().enumerate() {
+            response.push_str(&format!("{}={}\n", index, device.pf_guid));
+        }
+
+        (StatusCode::OK, response)
+    } else {
+        (StatusCode::NOT_FOUND, "devices not available".to_string())
+    }
+}
+
+async fn get_instances(
+    State(state): State<Arc<dyn InstanceMetadataReader>>,
+    Path(device_index): Path<usize>,
+) -> (StatusCode, String) {
+    let read_guard = state.read();
+    let metadata = match read_guard.as_ref() {
+        Some(metadata) => metadata,
+        None => {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "metadata currently unavailable".to_string(),
+            );
+        }
+    };
+
+    if let Some(devices) = &metadata.ib_devices {
+        if devices.len() <= device_index {
+            return (
+                StatusCode::NOT_FOUND,
+                format!("no device at index: {}", device_index),
+            );
+        }
+        let dev = &devices[device_index];
+
+        let mut response = String::new();
+        for (index, instance) in dev.instances.iter().enumerate() {
+            match &instance.ib_guid {
+                Some(guid) => response.push_str(&format!("{}={}\n", index, guid)),
+                None => continue,
+            }
+        }
+
+        (StatusCode::OK, response)
+    } else {
+        (StatusCode::NOT_FOUND, "devices not available".to_string())
+    }
+}
+
+async fn get_instance_attributes(
+    State(state): State<Arc<dyn InstanceMetadataReader>>,
+    Path((device_index, instance_index)): Path<(usize, usize)>,
+) -> (StatusCode, String) {
+    println!("Got here!");
+    let read_guard = state.read();
+    let metadata = match read_guard.as_ref() {
+        Some(metadata) => metadata,
+        None => {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "metadata currently unavailable".to_string(),
+            );
+        }
+    };
+
+    if let Some(devices) = &metadata.ib_devices {
+        if devices.len() <= device_index {
+            return (
+                StatusCode::NOT_FOUND,
+                format!("no device at index: {}", device_index),
+            );
+        }
+
+        let dev = &devices[device_index];
+
+        if dev.instances.len() <= instance_index {
+            return (
+                StatusCode::NOT_FOUND,
+                format!("no instance at index: {}", instance_index),
+            );
+        }
+        let inst = &dev.instances[instance_index];
+
+        let mut response = String::new();
+
+        if let Some(_ib_guid) = &inst.ib_guid {
+            response += &(GUID.to_owned() + "\n")
+        }
+        if let Some(_ib_partition_id) = &inst.ib_partition_id {
+            response += &(IB_PARTITION.to_owned() + "\n")
+        }
+        response.push_str(LID);
+
+        (StatusCode::OK, response)
+    } else {
+        (StatusCode::NOT_FOUND, "devices not available".to_string())
+    }
+}
+
+async fn get_instance_attribute(
+    State(state): State<Arc<dyn InstanceMetadataReader>>,
+    Path((device_index, instance_index, attribute)): Path<(usize, usize, String)>,
+) -> (StatusCode, String) {
+    let read_guard = state.read();
+    let metadata = match read_guard.as_ref() {
+        Some(metadata) => metadata,
+        None => {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "metadata currently unavailable".to_string(),
+            );
+        }
+    };
+
+    if let Some(devices) = &metadata.ib_devices {
+        if devices.len() <= device_index {
+            return (
+                StatusCode::NOT_FOUND,
+                format!("no device at index: {}", device_index),
+            );
+        }
+        let dev = &devices[device_index];
+
+        if dev.instances.len() <= instance_index {
+            return (
+                StatusCode::NOT_FOUND,
+                format!("no instance at index: {}", instance_index),
+            );
+        }
+        let inst = &dev.instances[instance_index];
+
+        return match attribute.as_str() {
+            GUID => match &inst.ib_guid {
+                Some(guid) => (StatusCode::OK, guid.clone()),
+                None => {
+                    return (
+                        StatusCode::NOT_FOUND,
+                        format!("guid not found at index: {}", instance_index),
+                    );
+                }
+            },
+            IB_PARTITION => match &inst.ib_partition_id {
+                Some(ib_partition_id) => (StatusCode::OK, ib_partition_id.to_string()),
+                None => {
+                    return (
+                        StatusCode::NOT_FOUND,
+                        format!("ib partition not found at index: {}", instance_index),
+                    );
+                }
+            },
+            LID => (StatusCode::OK, inst.lid.to_string()),
+            _ => (StatusCode::NOT_FOUND, "no such attribute".to_string()),
+        };
+    } else {
+        (StatusCode::NOT_FOUND, "devices not available".to_string())
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use crate::instance_metadata_fetcher::{InstanceMetadata, MockInstanceMetadataReader};
     use axum::http;
+    use rpc::Uuid;
+    use uuid::uuid;
+
+    use crate::instance_metadata_fetcher::{
+        IBDeviceConfig, IBInstanceConfig, InstanceMetadata, MockInstanceMetadataReader,
+    };
+
+    use super::*;
 
     async fn setup_server(
         metadata: Option<InstanceMetadata>,
@@ -104,12 +299,10 @@ mod tests {
 
         assert_eq!(response.status(), expected_code);
 
-        if expected_code == StatusCode::OK {
-            let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
-            let body_str = std::str::from_utf8(&body).unwrap();
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body_str = std::str::from_utf8(&body).unwrap();
 
-            assert_eq!(body_str, expected_body);
-        }
+        assert_eq!(body_str, expected_body);
     }
 
     #[tokio::test]
@@ -118,6 +311,7 @@ mod tests {
             address: "127.0.0.1".to_string(),
             hostname: "localhost".to_string(),
             user_data: "\"userData\": {\"data\": 0}".to_string(),
+            ib_devices: None,
         };
 
         let (server, server_port) = setup_server(Some(metadata.clone())).await;
@@ -137,6 +331,7 @@ mod tests {
             address: "127.0.0.1".to_string(),
             hostname: "localhost".to_string(),
             user_data: "\"userData\": {\"data\": 0}".to_string(),
+            ib_devices: None,
         };
 
         let (server, server_port) = setup_server(Some(metadata.clone())).await;
@@ -156,6 +351,7 @@ mod tests {
             address: "127.0.0.1".to_string(),
             hostname: "localhost".to_string(),
             user_data: "\"userData\": {\"data\": 0}".to_string(),
+            ib_devices: None,
         };
 
         let (server, server_port) = setup_server(Some(metadata.clone())).await;
@@ -175,8 +371,243 @@ mod tests {
         send_request_and_check_response(
             server_port,
             "hostname",
-            "",
+            "metadata currently unavailable",
             StatusCode::INTERNAL_SERVER_ERROR,
+        )
+        .await;
+        server.abort();
+    }
+
+    #[tokio::test]
+    async fn test_get_ib_devices() {
+        let metadata = InstanceMetadata {
+            address: "127.0.0.1".to_string(),
+            hostname: "localhost".to_string(),
+            user_data: "\"userData\": {\"data\": 0}".to_string(),
+            ib_devices: Some(vec![
+                IBDeviceConfig {
+                    pf_guid: "pfguid1".to_string(),
+                    instances: vec![IBInstanceConfig {
+                        ib_partition_id: None,
+                        ib_guid: Some("test-guid1".to_string()),
+                        lid: 0,
+                    }],
+                },
+                IBDeviceConfig {
+                    pf_guid: "pfguid2".to_string(),
+                    instances: vec![IBInstanceConfig {
+                        ib_partition_id: None,
+                        ib_guid: Some("test-guid2".to_string()),
+                        lid: 1,
+                    }],
+                },
+            ]),
+        };
+
+        let (server, server_port) = setup_server(Some(metadata.clone())).await;
+        send_request_and_check_response(
+            server_port,
+            "infiniband/devices",
+            "0=pfguid1\n1=pfguid2\n",
+            StatusCode::OK,
+        )
+        .await;
+        server.abort();
+    }
+
+    #[tokio::test]
+    async fn test_get_incorrect_ib_device() {
+        let metadata = InstanceMetadata {
+            address: "127.0.0.1".to_string(),
+            hostname: "localhost".to_string(),
+            user_data: "\"userData\": {\"data\": 0}".to_string(),
+            ib_devices: Some(vec![IBDeviceConfig {
+                pf_guid: "pfguid1".to_string(),
+                instances: vec![IBInstanceConfig {
+                    ib_partition_id: None,
+                    ib_guid: Some("test-guid1".to_string()),
+                    lid: 0,
+                }],
+            }]),
+        };
+
+        let (server, server_port) = setup_server(Some(metadata.clone())).await;
+        send_request_and_check_response(
+            server_port,
+            "infiniband/devices/2",
+            "no device at index: 2",
+            StatusCode::NOT_FOUND,
+        )
+        .await;
+        server.abort();
+    }
+
+    #[tokio::test]
+    async fn test_get_ib_instances() {
+        let metadata = InstanceMetadata {
+            address: "127.0.0.1".to_string(),
+            hostname: "localhost".to_string(),
+            user_data: "\"userData\": {\"data\": 0}".to_string(),
+            ib_devices: Some(vec![IBDeviceConfig {
+                pf_guid: "guid".to_string(),
+                instances: vec![
+                    IBInstanceConfig {
+                        ib_partition_id: None,
+                        ib_guid: Some("test-guid1".to_string()),
+                        lid: 0,
+                    },
+                    IBInstanceConfig {
+                        ib_partition_id: None,
+                        ib_guid: Some("test-guid2".to_string()),
+                        lid: 1,
+                    },
+                ],
+            }]),
+        };
+
+        let (server, server_port) = setup_server(Some(metadata.clone())).await;
+        send_request_and_check_response(
+            server_port,
+            "infiniband/devices/0/instances",
+            "0=test-guid1\n1=test-guid2\n",
+            StatusCode::OK,
+        )
+        .await;
+        server.abort();
+    }
+
+    #[tokio::test]
+    async fn test_get_ib_instance() {
+        let metadata = InstanceMetadata {
+            address: "127.0.0.1".to_string(),
+            hostname: "localhost".to_string(),
+            user_data: "\"userData\": {\"data\": 0}".to_string(),
+            ib_devices: Some(vec![IBDeviceConfig {
+                pf_guid: "guid".to_string(),
+                instances: vec![IBInstanceConfig {
+                    ib_partition_id: Some(Uuid::from(uuid!(
+                        "67e55044-10b1-426f-9247-bb680e5fe0c8"
+                    ))),
+                    ib_guid: Some("test-guid1".to_string()),
+                    lid: 0,
+                }],
+            }]),
+        };
+
+        let (server, server_port) = setup_server(Some(metadata.clone())).await;
+        send_request_and_check_response(
+            server_port,
+            "infiniband/devices/0/instances/0",
+            "guid\npartition\nlid",
+            StatusCode::OK,
+        )
+        .await;
+        server.abort();
+    }
+
+    #[tokio::test]
+    async fn test_get_ib_instance_not_all_attributes() {
+        let metadata = InstanceMetadata {
+            address: "127.0.0.1".to_string(),
+            hostname: "localhost".to_string(),
+            user_data: "\"userData\": {\"data\": 0}".to_string(),
+            ib_devices: Some(vec![IBDeviceConfig {
+                pf_guid: "guid".to_string(),
+                instances: vec![IBInstanceConfig {
+                    ib_partition_id: None,
+                    ib_guid: None,
+                    lid: 0,
+                }],
+            }]),
+        };
+
+        let (server, server_port) = setup_server(Some(metadata.clone())).await;
+        send_request_and_check_response(
+            server_port,
+            "infiniband/devices/0/instances/0",
+            "lid",
+            StatusCode::OK,
+        )
+        .await;
+        server.abort();
+    }
+
+    #[tokio::test]
+    async fn test_get_incorrect_ib_instance() {
+        let metadata = InstanceMetadata {
+            address: "127.0.0.1".to_string(),
+            hostname: "localhost".to_string(),
+            user_data: "\"userData\": {\"data\": 0}".to_string(),
+            ib_devices: Some(vec![IBDeviceConfig {
+                pf_guid: "guid".to_string(),
+                instances: vec![IBInstanceConfig {
+                    ib_partition_id: None,
+                    ib_guid: Some("test-guid1".to_string()),
+                    lid: 0,
+                }],
+            }]),
+        };
+
+        let (server, server_port) = setup_server(Some(metadata.clone())).await;
+        send_request_and_check_response(
+            server_port,
+            "infiniband/devices/0/instances/3",
+            "no instance at index: 3",
+            StatusCode::NOT_FOUND,
+        )
+        .await;
+        server.abort();
+    }
+
+    #[tokio::test]
+    async fn test_get_ib_instance_attribute() {
+        let metadata = InstanceMetadata {
+            address: "127.0.0.1".to_string(),
+            hostname: "localhost".to_string(),
+            user_data: "\"userData\": {\"data\": 0}".to_string(),
+            ib_devices: Some(vec![IBDeviceConfig {
+                pf_guid: "guid".to_string(),
+                instances: vec![IBInstanceConfig {
+                    ib_partition_id: None,
+                    ib_guid: Some("test-guid".to_string()),
+                    lid: 0,
+                }],
+            }]),
+        };
+
+        let (server, server_port) = setup_server(Some(metadata.clone())).await;
+        send_request_and_check_response(
+            server_port,
+            "infiniband/devices/0/instances/0/guid",
+            "test-guid",
+            StatusCode::OK,
+        )
+        .await;
+        server.abort();
+    }
+
+    #[tokio::test]
+    async fn test_get_ib_instance_nonexistent_attribute() {
+        let metadata = InstanceMetadata {
+            address: "127.0.0.1".to_string(),
+            hostname: "localhost".to_string(),
+            user_data: "\"userData\": {\"data\": 0}".to_string(),
+            ib_devices: Some(vec![IBDeviceConfig {
+                pf_guid: "guid".to_string(),
+                instances: vec![IBInstanceConfig {
+                    ib_partition_id: None,
+                    ib_guid: None,
+                    lid: 0,
+                }],
+            }]),
+        };
+
+        let (server, server_port) = setup_server(Some(metadata.clone())).await;
+        send_request_and_check_response(
+            server_port,
+            "infiniband/devices/0/instances/0/partition",
+            "ib partition not found at index: 0",
+            StatusCode::NOT_FOUND,
         )
         .await;
         server.abort();
--- bluefield/agent/src/instance_metadata_fetcher.rs
+++ bluefield/agent/src/instance_metadata_fetcher.rs
@@ -21,6 +21,8 @@ use tracing::{error, trace};
 
 use ::rpc::forge as rpc;
 use ::rpc::forge_tls_client::{self, ForgeClientConfig};
+use ::rpc::Instance;
+use ::rpc::Uuid as uuid;
 
 /// The instance metadata - as fetched from the
 /// Forge Site Controller
@@ -29,6 +31,20 @@ pub struct InstanceMetadata {
     pub address: String,
     pub hostname: String,
     pub user_data: String,
+    pub ib_devices: Option<Vec<IBDeviceConfig>>,
+}
+
+#[derive(Clone, Debug)]
+pub struct IBDeviceConfig {
+    pub pf_guid: String,
+    pub instances: Vec<IBInstanceConfig>,
+}
+
+#[derive(Clone, Debug)]
+pub struct IBInstanceConfig {
+    pub ib_partition_id: Option<uuid>,
+    pub ib_guid: Option<String>,
+    pub lid: u32,
 }
 
 /// An interface for reading the latest received network configuration for
@@ -200,9 +216,66 @@ async fn fetch_latest_ip_addresses(
         .and_then(|tenant_config| tenant_config.user_data.clone())
         .ok_or_else(|| eyre::eyre!("user data is not present in tenant config"))?;
 
+    let devices = match extract_instance_ib_config(first_instance) {
+        Ok(value) => Some(value),
+        Err(e) => {
+            trace!("Failed to fetch IB config: {}", e.to_string());
+            None
+        }
+    };
+
     Ok(InstanceMetadata {
         address: pf_address,
         hostname,
         user_data,
+        ib_devices: devices,
     })
+}
+
+fn extract_instance_ib_config(instance: &Instance) -> Result<Vec<IBDeviceConfig>, eyre::Error> {
+    let ib_config = instance
+        .config
+        .as_ref()
+        .and_then(|config| config.infiniband.as_ref())
+        .ok_or_else(|| eyre::eyre!("No infiniband interfaces found"))?;
+
+    let ib_interface_configs = &ib_config.ib_interfaces;
+
+    let ib_status = instance
+        .status
+        .as_ref()
+        .and_then(|status| status.infiniband.as_ref())
+        .ok_or_else(|| eyre::eyre!("No infiniband interfaces found"))?;
+
+    let ib_interface_statuses = &ib_status.ib_interfaces;
+
+    let mut devices: Vec<IBDeviceConfig> = Vec::new();
+
+    for (index, config) in ib_interface_configs.iter().enumerate() {
+        let status = &ib_interface_statuses[index];
+
+        let instance: IBInstanceConfig = IBInstanceConfig {
+            ib_partition_id: config.ib_partition_id.clone(),
+            ib_guid: status.guid.clone(),
+            lid: status.lid,
+        };
+
+        if let Some(pf_guid) = &status.pf_guid {
+            match devices.iter_mut().find(|dev| &(dev.pf_guid) == pf_guid) {
+                Some(device) => device.instances.push(instance),
+                None => devices.push(IBDeviceConfig {
+                    pf_guid: pf_guid.clone(),
+                    instances: vec![instance],
+                }),
+            }
+        } else {
+            continue;
+        }
+    }
+
+    if devices.is_empty() {
+        return Err(eyre::eyre!("No infiniband devices found"));
+    }
+
+    Ok(devices)
 }
