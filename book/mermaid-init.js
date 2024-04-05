d9e1a68a7c1a9a74e98c32581b16343ae38dcced
Matthias Einwag <meinwag@nvidia.com>
2024-04-05 11:27:52 -0700

    docs: Limit mermaid diagrams to page width and use neutral theme

--- book/mermaid-init.js
+++ book/mermaid-init.js
@@ -1,4 +1,16 @@
+// For the list of supported configs, reference
+// https://github.com/mermaid-js/mermaid/blob/master/packages/mermaid/src/config.type.ts
+
+// For certain diagrams, we want them to be scrollable.
+// TODO: Check whether this could be a css flag on the diagram itself
+var useMaxWidth = true
+if (window.location.pathname.includes('schema.html')) {
+    useMaxWidth = false;
+}
+
 mermaid.initialize({
     startOnLoad:true,
-    flowchart: { useMaxWidth: false }
+    flowchart: { useMaxWidth: useMaxWidth },
+    sequence: { useMaxWidth: useMaxWidth },
+    theme: 'neutral'
 });
