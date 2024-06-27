9772daaaff70825dc3545225c598eaabf7aa8d35
Peter Rudenko <prudenko@nvidia.com>
2024-06-27 00:50:44 -0700

    fix: reconfigure machine interface to secondary in site-explorer

--- api/src/api.rs
+++ api/src/api.rs
@@ -1010,7 +1010,6 @@ impl Forge for Api {
                 &mut txn,
                 Some(&hardware_info),
                 machine.id(),
-                true,
             )
             .await?;
 
