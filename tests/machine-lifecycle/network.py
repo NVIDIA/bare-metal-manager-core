3203c83a5a49207094559ff68a0e4e0be4141dc5
Daryl Blanc <dblanc@nvidia.com>
2024-08-21 05:41:27 -0700

    chore: add some extra state debugging to the machine lifecycle test

--- tests/machine-lifecycle/network.py
+++ tests/machine-lifecycle/network.py
@@ -1,5 +1,5 @@
 """Things we might want to do with a Forge Instance."""
-
+import datetime
 import socket
 import time
 
@@ -10,17 +10,17 @@ def wait_for_host_port(hostname: str, port: int, max_retries: int = 20, sleep_ti
     Sleep for `sleep_time` in between attempts, up to `max_retries`.
     :raises TimeoutError: if not successful within `max_retries`
     """
-    print(f"Attempting to connect to {hostname} port {port} up to {max_retries} times with {sleep_time=}")
+    print(f"{datetime.datetime.now(datetime.UTC)}: Attempting to connect to {hostname} port {port} up to {max_retries} times with {sleep_time=}")
     retry_count = 0
     while retry_count < max_retries:
         # Check if the port is up on the host: 0 for yes 1 for no
         if _test_host_port(hostname, port) == 0:
             return
         retry_count += 1
-        print(f"Unable to connect to {hostname} port {port} after {retry_count} attempts")
+        print(f"{datetime.datetime.now(datetime.UTC)}: Unable to connect to {hostname} port {port} after {retry_count} attempts")
         time.sleep(sleep_time)
     else:
-        raise TimeoutError(f"Unable to contact {hostname} port {port}")
+        raise TimeoutError(f"{datetime.datetime.now(datetime.UTC)}: Unable to contact {hostname} port {port}")
 
 
 def _test_host_port(host: str, port: int) -> int:
