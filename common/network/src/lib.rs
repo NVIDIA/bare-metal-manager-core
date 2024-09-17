066cce5bee4144fe3f5e32b94ad38aa020b04752
Chet Nichols III <chetn@nvidia.com>
2024-09-17 14:53:35 -0700

    fix(api): common mac address deserializer for mlx ch:64 + empty macs
    https://jirasw.nvidia.com/browse/FORGE-4373
    
    So there's our friend `ch:64`, but there's also a case where we'll just get empty MAC addresses back (as in `mac_address: ""`), so at this point, lets just `deserialize_with` something common, and lets put that something common into `forge_network` so it can be shared.
    
    This refactors `derserialize_ch_64` into a `deserialize_mlx_mac` (for input `MacAddress` values), and `deserialize_optional_mlx_mac` (for `Option<MacAddress>` values), and puts it into `common/network/`.
    
    See merge request nvmetal/carbide!2940

--- common/network/src/lib.rs
+++ common/network/src/lib.rs
@@ -10,7 +10,9 @@
  * its affiliates is strictly prohibited.
  */
 
-use mac_address::MacAddress;
+use mac_address::{MacAddress, MacParseError};
+use serde::de::Deserializer;
+use serde::Deserialize;
 use std::str::FromStr;
 
 /// virtualization is a module specific to shared code around
@@ -20,6 +22,16 @@ use std::str::FromStr;
 pub mod virtualization;
 
 const STRIPPED_MAC_LENGTH: usize = 12;
+
+/// MELLANOX_SF_VF_MAC_ADDRESS_IN exists to really make it obvious
+/// that the MAC address reported to topology data for SFs and VFs
+/// comes in as ch:64.
+pub const MELLANOX_SF_VF_MAC_ADDRESS_IN: &str = "ch:64";
+
+/// MELLANOX_SF_VF_MAC_ADDRESS_OUT exists to really make it obvious
+/// that we take MELLANOX_SF_VF_MAC_ADDRESS_IN and rewrite it out
+/// as this.
+pub const MELLANOX_SF_VF_MAC_ADDRESS_OUT: &str = "00:00:00:00:00:64";
 
 /// sanitized_mac takes a potentially nasty input MAC address
 /// string (e.g. `"a088c2    460c68"`, cleans up anything that
@@ -68,9 +80,82 @@ pub fn sanitized_mac(input_mac: String) -> eyre::Result<MacAddress> {
     MacAddress::from_str(&sanitized_mac).map_err(|e| eyre::eyre!("Failed to initialize MacAddress from sanitized MAC: {} (input: {}, stripped: {}, sanitized: {}", e, input_mac, stripped_mac, sanitized_mac))
 }
 
+/// deserialize_mlx_mac exists due to an interesting behavior
+/// of Mellanox cards -- SFs and VFs (i.e. interfaces that aren't
+/// the physical interface) report a MAC address of "ch:64",
+/// which isn't a real MAC address. Unfortnuately, this breaks
+/// MAC address validation + serialization for everyone else for
+/// this field.
+///
+/// In other cases, it will report an empty string, so that's
+/// another case we need to deal with.
+///
+/// So, instead of doing away with validation entirely, this
+/// custom deserialization function exists to rewrite ch:64 as
+/// 00:::::64 -- this is used for both ingestion (as in, when
+/// topology data is sent to us as JSON), and for reading legacy
+/// data from the database; at this point, serialization out to
+/// the database will ALWAYS be a valid MAC, since the field is
+/// a MacAddress now, so we just care about deserialization.
+///
+/// Fwiw, we obviously don't use ch:64 as an actual MAC
+/// address, but still want us some insight in topology
+/// data that its a special case, while still meeting the
+/// requirements of being a valid MAC address.
+pub fn deserialize_mlx_mac<'a, D>(deserializer: D) -> Result<MacAddress, D::Error>
+where
+    D: Deserializer<'a>,
+{
+    let input_value = String::deserialize(deserializer)?;
+    let mac_address = deserialize_input_mac_to_address(&input_value).map_err(|e| {
+        serde::de::Error::custom(format!(
+            "failed to parse input mac_address({}): {}",
+            input_value, e
+        ))
+    })?;
+
+    Ok(mac_address)
+}
+
+pub fn deserialize_optional_mlx_mac<'a, D>(deserializer: D) -> Result<Option<MacAddress>, D::Error>
+where
+    D: Deserializer<'a>,
+{
+    let optional_value: Option<String> = Option::deserialize(deserializer)?;
+
+    let mac_address: Option<MacAddress> = match optional_value {
+        Some(input_value) => {
+            let mac_address = deserialize_input_mac_to_address(&input_value).map_err(|e| {
+                serde::de::Error::custom(format!(
+                    "failed to parse input mac_address({}): {}",
+                    input_value, e
+                ))
+            })?;
+            Some(mac_address)
+        }
+        None => None,
+    };
+
+    Ok(mac_address)
+}
+
+/// deserialize_input_mac_to_address is a common input to MAC conversion
+/// function used by deserialize_mlx_mac and deserialize_optional_mlx_mac.
+fn deserialize_input_mac_to_address(input_value: &String) -> Result<MacAddress, MacParseError> {
+    let mac_string = if input_value == MELLANOX_SF_VF_MAC_ADDRESS_IN {
+        MELLANOX_SF_VF_MAC_ADDRESS_OUT.to_string()
+    } else if input_value.is_empty() {
+        "00:00:00:00:00:00".to_string()
+    } else {
+        input_value.clone()
+    };
+
+    let mac_address: MacAddress = mac_string.parse()?;
+    Ok(mac_address)
+}
 #[cfg(test)]
 mod tests {
-    use super::sanitized_mac;
+    use super::{deserialize_input_mac_to_address, sanitized_mac, MELLANOX_SF_VF_MAC_ADDRESS_OUT};
 
     #[test]
     fn test_gross_redfish_mac() {
@@ -112,5 +197,26 @@ mod tests {
     fn test_too_long_mac() {
         let too_long_mac = "aabbccddeeffgg00112233445566778899".to_string();
         assert!(sanitized_mac(too_long_mac).is_err());
+    }
+
+    #[test]
+    fn test_deserialize_happy_mac() {
+        let happy_mac = "00:11:22:33:44:55".to_string();
+        let mac_address = deserialize_input_mac_to_address(&happy_mac).unwrap();
+        assert_eq!(happy_mac, mac_address.to_string());
+    }
+
+    #[test]
+    fn test_deserialize_ch64_mac() {
+        let silly_mac = "ch:64".to_string();
+        let mac_address = deserialize_input_mac_to_address(&silly_mac).unwrap();
+        assert_eq!(MELLANOX_SF_VF_MAC_ADDRESS_OUT, mac_address.to_string());
+    }
+
+    #[test]
+    fn test_deserialize_empty_mac() {
+        let empty_mac = "".to_string();
+        let mac_address = deserialize_input_mac_to_address(&empty_mac).unwrap();
+        assert_eq!("00:00:00:00:00:00", mac_address.to_string());
     }
 }
