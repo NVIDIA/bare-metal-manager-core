f65954d6dea97a9fae2512a0eb885c1440753c5c
Laura Gray <lgray@nvidia.com>
2025-05-16 05:22:58 -0700

    MLT: Add config for downgrading to DOCA 2.2
    For HFA site bring-up we need to test ingestion of DOCA 2.2 DPUs.
    
    Adding config for testing both 2.2.0 and 2.2.1 as we don't yet know which version is installed there.
    
    We have been told there are Bluefield-3s with DOCA 2.2.0 but the documentation says this is an unsupported combination. For now we will test and see.
    
    See merge request nvmetal/carbide!4085

--- tests/machine-lifecycle/config.py
+++ tests/machine-lifecycle/config.py
@@ -4,7 +4,10 @@ WAIT_FOR_READY = 60 * 180
 WAIT_FOR_INSTANCE = 60 * 45
 WAIT_FOR_HOSTINIT_AFTER_DOWNGRADE = 60 * 150
 
+
+###################################
 ### DPU Firmware Configurations ###
+###################################
 
 # Expected versions to which Forge should auto-upgrade (currently DOCA 2.8.0 / HBN 2.3.0)
 BFB_VERSION_UP = "2.8.0"
@@ -12,19 +15,31 @@ NIC_VERSION_UP = "32.42.1000"
 BMC_VERSION_UP = "24.07"
 CEC_VERSION_UP = "00.02.0182.0000"
 
-# DOCA 2.2.0 / HBN 1.5.0 (HFA site bring-up version)
-# HBN_1_5_0 = {
-#     "BFB_VERSION": "2.2.0",
-#     "NIC_VERSION": "32.38.1002",
-#     "BMC_VERSION": "23.07",
-#     "CEC_VERSION": "",
-#     "BFB_URL": "",
-#     "NIC_FW_URL": "",
-#     "BMC_FW_URL": "",
-#     "CEC_FW_URL": "",
-# }
+# HFA site bring-up versions. TBD which of the below 2 is installed on the HFA site, for now we will test both
+# DOCA 2.2.0 / HBN 1.5.0 (supposedly only supports BF-2 but we have BF-3s in HFA)
+HBN_1_5_0_a = {
+    "BFB_VERSION": "2.2.0",
+    "NIC_VERSION": "32.38.1002",
+    "BMC_VERSION": "23.07",
+    "CEC_VERSION": "00.02.0127.0000",
+    "BFB_URL": "https://urm.nvidia.com/artifactory/sw-mlnx-bluefield-generic/Ubuntu22.04/DOCA_2.2.0_BSP_4.2.0_Ubuntu_22.04-2.23-07.prod.bfb",
+    "NIC_FW_URL": "https://www.mellanox.com/downloads/firmware/fw-BlueField-3-rel-32_38_1002-900-9D3B6-00CV-A_Ax-NVME-20.4.1-UEFI-21.4.10-UEFI-22.4.10-UEFI-14.31.20-FlexBoot-3.7.201.signed.bin.zip",
+    "BMC_FW_URL": "https://urm.nvidia.com/artifactory/sw-bmc-generic-local/BF3/BF3BMC-23.07-3/OPN/bf3-bmc-23.07-3_opn.fwpkg",
+    "CEC_FW_URL": "https://urm.nvidia.com/artifactory/sw-bmc-generic-local/Glacier/00.02.0127.0000/sign/n02/rel-prod/cec1736-ecfw-00.02.0127.0000-n02-rel-prod.fwpkg",
+}
+# DOCA 2.2.1 / HBN 1.5.0
+HBN_1_5_0_b = {
+    "BFB_VERSION": "2.2.1",
+    "NIC_VERSION": "32.38.3056",
+    "BMC_VERSION": "23.09",
+    "CEC_VERSION": "00.02.0127.0000",
+    "BFB_URL": "https://urm.nvidia.com/artifactory/sw-mlnx-bluefield-generic/Ubuntu22.04/DOCA_2.2.1_BSP_4.2.2_Ubuntu_22.04-13.23-09.prod.bfb",
+    "NIC_FW_URL": "https://www.mellanox.com/downloads/firmware/fw-BlueField-3-rel-32_38_3056-900-9D3B6-00CV-A_Ax-NVME-20.4.1-UEFI-21.4.12-UEFI-22.4.12-UEFI-14.31.22-FlexBoot-3.7.201.signed.bin.zip",
+    "BMC_FW_URL": "https://urm.nvidia.com/artifactory/sw-bmc-generic-local/BF3/BF3BMC-23.09-6/OPN/bf3-bmc-23.09-6_opn.fwpkg",
+    "CEC_FW_URL": "https://urm.nvidia.com/artifactory/sw-bmc-generic-local/Glacier/00.02.0127.0000/sign/n02/rel-prod/cec1736-ecfw-00.02.0127.0000-n02-rel-prod.fwpkg",
+}
 
-# DOCA 2.5.0 / HBN 2.0.0 (minimum supported by Forge?)
+# DOCA 2.5.0 / HBN 2.0.0
 # Note: only DOCA 2.7.0 and higher include the BMC, CEC, and NIC in the BFB
 HBN_2_0_0 = {
     "BFB_VERSION": "2.5.0",
