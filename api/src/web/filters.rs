82aba75b73477450abcc414afc0aa444adcef3cc
Matthias Einwag <meinwag@nvidia.com>
2025-06-24 21:06:34 -0700

    feat: Link to state handler return location on gitlab from carbide-web
    Instead of just showing the source code file name and line number, we can directly render a gitlab link around it. That will allow operators to click on the link to examine the code that triggered the state handlers decision.
    
    See merge request nvmetal/carbide!4197

--- api/src/web/filters.rs
+++ api/src/web/filters.rs
@@ -229,10 +229,24 @@ pub fn controller_state_reason_fmt(
     }
 
     if let Some(source_ref) = reason.source_ref.as_ref() {
+        const GITLAB_REPO: &str = "https://gitlab-master.nvidia.com/nvmetal/carbide";
+
+        let git_sha = forge_version::v!(git_sha);
+        let commit_hash = if !git_sha.is_empty() {
+            git_sha
+        } else {
+            "trunk"
+        };
+
         write!(
             &mut result,
-            "<br><b>Source:</b> {}:{}",
-            source_ref.file, source_ref.line
+            "<br><b>Source:</b> <a href=\"{}/-/blob/{}/{}#L{}\">{}:{}</a>",
+            GITLAB_REPO,
+            commit_hash,
+            source_ref.file,
+            source_ref.line,
+            source_ref.file,
+            source_ref.line
         )
         .unwrap();
     }
