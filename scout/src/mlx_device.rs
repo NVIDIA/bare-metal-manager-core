22c0b1ffd26047e94a2cda7ab481e7ecbaaecf8b
Chet Nichols III <chetn@nvidia.com>
2025-09-21 22:17:00 -0700

    move MlxDeviceReport publishing outside of registration
    ## Description
    
    It was in registration to start, but then I realized it makes more
    sense to have it **after** registration (and after `initial_setup`),
    since this is more closely aligned without publishing will
    actually be once it's a part of the `Action` feedback loop,
    and not part of registration.
    
    Auth policies are already in place.
    
    ## Type of Change
    <!-- Check one that best describes this MR -->
    - [ ] **Add** - New feature or capability
    - [ ] **Change** - Changes in existing functionality  
    - [ ] **Fix** - Bug fixes
    - [ ] **Remove** - Removed features or deprecated functionality
    - [x] **Internal** - Internal changes (refactoring, tests, docs, etc.)
    
    ## Related Tickets (Optional)
    
    ## Breaking Changes
    - [ ] This MR contains breaking changes
    
    <!-- If checked above, describe the breaking changes and migration steps -->
    
    ## Testing
    <!-- How was this tested? Check all that apply -->
    - [ ] Unit tests added/updated
    - [ ] Integration tests added/updated  
    - [ ] Manual testing performed
    - [ ] No testing required (docs, internal refactor, etc.)
    
    ## Additional Notes
    <!-- Any additional context, deployment notes, or reviewer guidance -->
    
    See merge request nvmetal/carbide!4621

--- scout/src/mlx_device.rs
+++ scout/src/mlx_device.rs
@@ -10,19 +10,46 @@
  * its affiliates is strictly prohibited.
  */
 
+use crate::cfg::Options;
+use crate::client;
 use ::rpc::protos::mlx_device::{
     MlxDeviceReport as MlxDeviceReportPb, PublishMlxDeviceReportRequest,
+    PublishMlxDeviceReportResponse,
 };
 use mlxconfig_device::report::MlxDeviceReport;
+use scout::CarbideClientResult;
 
 // create_device_report_request is a one stop shop to collect
 // Mellanox device data from the machine, create a report, convert
 // it into the underlying protobuf type, and then return a request
 // instance to publish to carbide-api.
 pub fn create_device_report_request() -> Result<PublishMlxDeviceReportRequest, String> {
+    tracing::info!("creating PublishMlxDeviceReportRequest");
     let report = MlxDeviceReport::new().collect()?;
     let report_pb: MlxDeviceReportPb = report.into();
     Ok(PublishMlxDeviceReportRequest {
         report: Some(report_pb),
     })
+}
+
+// publish_mlx_device_report is used to publish an MlxDeviceReport for the current
+// machine, which will collect the hardware + firmware/version details of all Mellanox
+// devices on the machine, including DPUs and DPAs. This is then published to carbide-api,
+// which leverages this data for ensuring devices are synced with the correct mlxconfig
+// settings, and have been (or will be instructed to) updated to the correct firmware version.
+//
+// When called from scout on a host, a report will contain *all* Mellanox devices on the host.
+// When called from the agent on a DPU, a report will contain *only* the DPU its being called from.
+pub async fn publish_mlx_device_report(
+    config: &Options,
+    req: PublishMlxDeviceReportRequest,
+) -> CarbideClientResult<PublishMlxDeviceReportResponse> {
+    tracing::info!("sending PublishMlxDeviceReportRequest: {req:?}");
+    let request = tonic::Request::new(req);
+    let mut client = client::create_forge_client(config).await?;
+    let response = client
+        .publish_mlx_device_report(request)
+        .await?
+        .into_inner();
+    Ok(response)
 }
