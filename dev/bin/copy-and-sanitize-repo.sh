22f64f5441695636c6e30b6dc26822eca2ef0174
Ken Simon <ksimon@nvidia.com>
2025-10-17 08:21:22 -0700

    Add a script that copies the carbide repo while sanitizing the result
    ## Description
    This is for sharing the carbide repo with mirantis, for which the
    agreement is that we'll show them the carbide code but won't provide any
    support. (The expectation is that they'll read the code but likely not
    try to run it as-is)
    
    This is a separate step from the eventual open-sourcing of carbide,
    which will require other changes to the code first. The mirantis sharing
    is much shorter term and merely requires a sanitized repo.
    
    ## Type of Change
    - [ ] **Add** - New feature or capability
    - [ ] **Change** - Changes in existing functionality  
    - [ ] **Fix** - Bug fixes
    - [ ] **Remove** - Removed features or deprecated functionality
    - [X] **Internal** - Internal changes (refactoring, tests, docs, etc.)
    
    ## Related Tickets (Optional)
    
    JIRA: [FORGE-7078](https://jirasw.nvidia.com/browse/FORGE-7078)
    
    ## Breaking Changes
    - [ ] This MR contains breaking changes
    
    ## Testing
    - [ ] Unit tests added/updated
    - [ ] Integration tests added/updated  
    - [ ] Manual testing performed
    - [X] No testing required (docs, internal refactor, etc.)
    
    ## Additional Notes
    The sanitized repo can be found at https://gitlab-master.nvidia.com/nvmetal/carbide-mirror-mirantis
    
    I'm committing the script itself so that we can iterate on which things should be excluded, and anticipating that we may need to snapshot carbide again if there's changes we need to make in the code first.
    
    See merge request nvmetal/carbide!4733

---
+++ dev/bin/copy-and-sanitize-repo.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+set -euo pipefail
+
+: "${INITIAL_BRANCH:=main}"
+
+cat <<-EOF
+================================================================================
+| This script will take the entire contents of the carbide repo and copy it to |
+| a new repo, while sanitizing things by removing anything sensitive from the  |
+| result. The goal is to deliver a copy of the code for carbide for partners   |
+| to inspect (not necessarily as a supported configuration, mostly just for    |
+| code reading.)                                                               |
+|                                                                              |
+| The resulting sanitized copy should build properly but otherwise there are   |
+| no guarantees about whether it will work in a real environment.              |
+================================================================================
+EOF
+
+if [[ -z "${1:-}" ]]
+then
+    echo "Usage: $0 <dest-dir>"
+    exit 1
+fi
+
+# Canonicalize $DEST path before cd to repo root
+DEST="$(realpath "${1}")"
+
+# cd to repo root
+cd "$(dirname "${0}")/../.."
+
+# Ensure we're where we think we are
+if ! grep -q carbide-api Cargo.lock
+then
+    echo "Looks like this script is not in the carbide repo? Aborting."
+    exit 1
+fi
+
+# Make sure we're not copying into this repo
+if [[ "${DEST}" == "$(realpath "$(pwd)")/*" ]]
+then
+    echo "${DEST} is a subdirectory of this repo. Aborting."
+    exit 1
+fi
+
+if [[ -e "${DEST}" ]]
+then
+    # It already exists - make sure it already has a .git directory
+    if [[ ! -e "${DEST}/.git" ]]
+    then
+        cat <<-EOM
+${DEST} does not have a git directory, this is likely a mistake. Either let
+this script create a new directory, or point it at an existing git repo.
+
+Aborting.
+EOM
+        exit 1
+    fi
+
+    echo "WARNING: The contents of ${DEST} will be removed, and only the .git directory (if any) will be kept. Press \"y\" to continue."
+    read -sN 1 confirm
+    if [[ "${confirm}" != "y" ]]
+    then
+        echo "Aborting."
+        exit 1
+    fi
+else
+    echo "Creating git repo in ${DEST}"
+    mkdir -p "${DEST}"
+    (cd "${DEST}" && git init --initial-branch "${INITIAL_BRANCH}")
+fi
+
+
+# Clear the contents of $DEST
+(
+    cd "${DEST}"
+    find "${DEST}" -mindepth 1 -maxdepth 1 -not -path "${DEST}/.git" -a -not -name . -print0 | xargs -r0 rm -rf
+)
+
+# Copy from what's in git, excluding from the deny-list
+rsync -a \
+    --exclude-from=dev/sanitize-denylist.txt \
+    --files-from=<(git -c core.quotepath=off ls-files --recurse-submodules) \
+    . \
+    "${DEST}"/
+
+# Copy the .git dir of each module
+for submodule in pxe/ipxe/upstream pxe/mkosi
+do
+    mkdir -p "${DEST}/${submodule}" && cp -a "${submodule}/.git" "${DEST}/${submodule}"
+done
+
+# Create our own readme without a bunch of jira links
+cat >"${DEST}/README.md" <<-EOF
+# Carbide - Bare Metal Provisioning
+
+Carbide is a bare metal provisioning system used to manage the lifecycle of
+bare metal machines.
+EOF
+
+cat <<-EOM
+Repo copied. You can now run \`git add -A && git commit -m "Initial commit"\` or similar inside ${DEST}.
+EOM
