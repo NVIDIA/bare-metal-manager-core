b627a07ec7df6ec9df24be6de0344972bf7d2dc0
Patrice Breton <pbreton@nvidia.com>
2025-12-22 11:44:27 -0800

    Revert "Merge branch 'task/FORGE-7473-Add-batch-calls-for-expected-machines' into 'trunk'"
    ## Description
    Not a good time to break stuff and for some reason this is a problem despite passing tests.
    
    See merge request nvmetal/carbide!5049

--- crates/api-db/src/expected_machine.rs
+++ crates/api-db/src/expected_machine.rs
@@ -101,8 +101,7 @@ pub async fn find_one_linked(
  em.bmc_mac_address,
  mi.id AS interface_id,
  host(ee.address) AS address,
- mt.machine_id,
- em.id AS expected_machine_id
+ mt.machine_id
 FROM expected_machines em
  LEFT JOIN machine_interfaces mi ON em.bmc_mac_address = mi.mac_address
  LEFT JOIN machine_interface_addresses mia ON mi.id = mia.interface_id
@@ -133,8 +132,7 @@ pub async fn find_all_linked(txn: &mut PgConnection) -> DatabaseResult<Vec<Linke
  em.bmc_mac_address,
  mi.id AS interface_id,
  host(ee.address) AS address,
- mt.machine_id,
- em.id AS expected_machine_id
+ mt.machine_id
 FROM expected_machines em
  LEFT JOIN machine_interfaces mi ON em.bmc_mac_address = mi.mac_address
  LEFT JOIN machine_interface_addresses mia ON mi.id = mia.interface_id
