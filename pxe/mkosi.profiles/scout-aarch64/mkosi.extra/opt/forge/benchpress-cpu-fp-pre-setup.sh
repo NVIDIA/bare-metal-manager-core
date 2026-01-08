26b4428ea695546a8f923288c2b45b89cba795db
Ian Anderson <ianderson@nvidia.com>
2026-01-08 13:38:05 -0800

    fix: remove proprietary and nvidia specific details from mkosi profiles
    ## Description
    
          * Proprietary and nvidia specific code was moved to
            https://gitlab-master.nvidia.com/nvmetal/carbide-extras
          * gitlab-ci will handle cloning and adding the binaries/scripts
            to the mkosi profiles before mkosi runs
    
    ## Type of Change
    <!-- Check one that best describes this MR -->
    - [ ] **Add** - New feature or capability
    - [ ] **Change** - Changes in existing functionality  
    - [ ] **Fix** - Bug fixes
    - [ ] **Remove** - Removed features or deprecated functionality
    - [X] **Internal** - Internal changes (refactoring, tests, docs, etc.)
    
    ## Related Tickets (Optional)
    <!-- If applicable, provide JIRA or/and NVBugs ticket. If none, the MR itself will be referenced in changelog -->
    JIRA: <!-- e.g., [FORGE-1234](https://jirasw.nvidia.com/browse/FORGE-1234) or leave blank -->
    NVBugs: <!-- e.g., [NVBugs-12345](https://nvbugspro.nvidia.com/bug/12345) or leave blank -->
    
    ## Breaking Changes
    - [ ] This MR contains breaking changes
    
    <!-- If checked above, describe the breaking changes and migration steps -->
    
    ## Testing
    <!-- How was this tested? Check all that apply -->
    - [ ] Unit tests added/updated
    - [ ] Integration tests added/updated  
    - [ ] Manual testing performed
    - [X] No testing required (docs, internal refactor, etc.)
    
    ## Additional Notes
    <!-- Any additional context, deployment notes, or reviewer guidance -->
    
    We cannot distribute software such as benchpress and nvinit does not apply outside nvidia
    
    See merge request nvmetal/carbide!5034

--- pxe/mkosi.profiles/scout-aarch64/mkosi.extra/opt/forge/benchpress-cpu-fp-pre-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-cd /opt/benchpress && ./setup.sh || exit 1
-
-cd /opt/benchpress && ./benchpress install cpu_2017_fp_rate_light || exit 1
--- pxe/mkosi.profiles/scout-x86_64/mkosi.extra/opt/forge/benchpress-fio-ssd-pre-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
- 
-cd /opt/benchpress && ./setup.sh || exit 1
-
-cd /opt/benchpress && ./benchpress install fio_ssd || exit 1
