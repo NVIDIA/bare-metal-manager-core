b4c3bc1d14b7dd438d3df0f73a692a83b1e3cd94
ianisimov <ianisimov@nvidia.com>
2026-01-28 10:23:03 -0800

    fmt fixes

--- crates/health/src/config.rs
+++ crates/health/src/config.rs
@@ -237,7 +237,7 @@ pub struct FirmwareCollectorConfig {
 impl Default for FirmwareCollectorConfig {
     fn default() -> Self {
         Self {
-            firmware_refresh_interval: Duration::from_secs(60*60*2), // 2 hours
+            firmware_refresh_interval: Duration::from_secs(60 * 60 * 2), // 2 hours
         }
     }
 }
