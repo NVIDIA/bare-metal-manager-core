e7ae9821034f0564d62dad0d612eb52590953e09
Matthias Einwag <meinwag@nvidia.com>
2026-01-30 22:11:17 +0000

    chore: Remove unused fields in NetworkPrefix
    These fields were not used by anything and never populated.
    
    Signed-off-by: Matthias Einwag <meinwag@nvidia.com>

--- crates/admin-cli/src/rpc.rs
+++ crates/admin-cli/src/rpc.rs
@@ -810,8 +810,6 @@ impl ApiClient {
                 prefix,
                 gateway,
                 reserve_first: 0,
-                state: None,
-                events: vec![],
                 free_ip_count: 1,
                 svi_ip: None,
             }],
