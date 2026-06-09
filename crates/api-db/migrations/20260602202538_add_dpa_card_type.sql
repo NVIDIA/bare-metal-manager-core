ae5d9efefbe54a736f84cdf9ebc1374a0a439aa7
Srinivasa Murthy <srmurthy@nvidia.com>
2026-06-09 20:57:27 +0000

    feat: Initial changes for Astra support
    Signed-off-by: Srinivasa Murthy <srmurthy@nvidia.com>

--- crates/api-db/migrations/20260602202538_add_dpa_card_type.sql
+++ crates/api-db/migrations/20260602202538_add_dpa_card_type.sql
@@ -2,4 +2,9 @@
 CREATE TYPE dpa_interface_type AS ENUM ('Svpc', 'Astra');
 
 ALTER TABLE dpa_interfaces
-    ADD COLUMN interface_type dpa_interface_type NOT NULL;
+    ADD COLUMN interface_type dpa_interface_type;
+
+UPDATE dpa_interfaces SET interface_type 'Svpc';
+
+ALTER TABLE dpa_interfaces
+    ALTER COLUMN interface_type SET NOT NULL;
