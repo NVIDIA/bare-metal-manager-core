hatamzad-nv/infra-controller

fix: Decode DPU network policy overrides (#3866)
Fixes an HTTP 500 when retrieving attached DPU details for machines with
network security policy overrides by preserving protobuf fields through
the Temporal workflow. The REST response stays the same, and the API
documentation now makes clear that only selected DPU network
configuration fields are exposed.

## Related issues

None.

## Type of Change

- [ ] **Add** - New feature or capability
- [ ] **Change** - Changes in existing functionality
- [x] **Fix** - Bug fixes
- [ ] **Remove** - Removed features or deprecated functionality
- [ ] **Internal** - Internal changes (refactoring, tests, docs, etc.)

## Breaking Changes

- [ ] **This PR contains breaking changes**

## Testing

- [x] Unit tests added/updated
- [ ] Integration tests added/updated
- [x] Manual testing performed
- [ ] No testing required (docs, internal refactor, etc.)

rest-api/api/pkg/api/handler/machine.go
@@ -19,6 +19,7 @@ import (
 
 	"go.opentelemetry.io/otel/attribute"
 	temporalClient "go.temporal.io/sdk/client"
+	tsdkConverter "go.temporal.io/sdk/converter"
 	tp "go.temporal.io/sdk/temporal"
 
 	"github.com/google/uuid"
@@ -2123,8 +2124,15 @@ func (gadmh GetAllDpuMachineHandler) Handle(c echo.Context) error {
 	logger.Info().Str("Workflow ID", wid).Msg("executed synchronous GetDpuMachines workflow")
 
 	// Block until the workflow has completed and returned success/error.
-	var controllerDpuMachines []*corev1.DpuMachine
+	var controllerDpuMachines corev1.DpuMachineList
 	wferr := we.Get(wfCtx, &controllerDpuMachines)
+	if errors.Is(wferr, tsdkConverter.ErrUnableToDecode) {
+		var legacyDpuMachines []*corev1.DpuMachine
+		wferr = we.Get(wfCtx, &legacyDpuMachines)
+		if wferr == nil {
+			controllerDpuMachines.Machines = legacyDpuMachines
+		}
+	}
 	if wferr != nil {
 		var timeoutErr *tp.TimeoutError
 		if errors.As(wferr, &timeoutErr) || wferr == context.DeadlineExceeded || wfCtx.Err() != nil {
@@ -2136,7 +2144,7 @@ func (gadmh GetAllDpuMachineHandler) Handle(c echo.Context) error {
 		return cutil.NewAPIErrorResponse(c, code, fmt.Sprintf("Failed to retrieve Machine DPU information from Site: %s", uwerr), nil)
 	}
 
-	apiDpuMachines = model.NewAPIDpuMachines(controllerDpuMachines, model.APIDpuMachineProtoContext{
+	apiDpuMachines = model.NewAPIDpuMachines(controllerDpuMachines.GetMachines(), model.APIDpuMachineProtoContext{
 		HostMachineID:            mID,
 		SiteID:                   site.ID,
 		InfrastructureProviderID: site.InfrastructureProviderID,

rest-api/api/pkg/api/handler/machine_test.go
@@ -36,6 +36,7 @@ import (
 
 	"go.temporal.io/api/enums/v1"
 	temporalClient "go.temporal.io/sdk/client"
+	tsdkConverter "go.temporal.io/sdk/converter"
 	tmocks "go.temporal.io/sdk/mocks"
 	tp "go.temporal.io/sdk/temporal"
 
@@ -3227,6 +3228,21 @@ func TestMachineHandler_Delete(t *testing.T) {
 	}
 }
 
+func TestGetDpuMachinesLegacyTemporalPayload(t *testing.T) {
+	legacyDpuMachines := []*corev1.DpuMachine{{Machine: &corev1.Machine{Id: &corev1.MachineId{Id: uuid.NewString()}}}}
+	dataConverter := tsdkConverter.GetDefaultDataConverter()
+	payloads, err := dataConverter.ToPayloads(legacyDpuMachines)
+	require.NoError(t, err)
+
+	var currentResult corev1.DpuMachineList
+	err = dataConverter.FromPayloads(payloads, &currentResult)
+	require.ErrorIs(t, err, tsdkConverter.ErrUnableToDecode)
+
+	var decodedLegacyResult []*corev1.DpuMachine
+	require.NoError(t, dataConverter.FromPayloads(payloads, &decodedLegacyResult))
+	require.Equal(t, legacyDpuMachines, decodedLegacyResult)
+}
+
 func TestMachineHandler_GetDpuMachines(t *testing.T) {
 	ctx := context.Background()
 	dbSession := testMachineInitDB(t)
@@ -3317,7 +3333,7 @@ func TestMachineHandler_GetDpuMachines(t *testing.T) {
 	cfg := common.GetTestConfig()
 
 	// Mock Temporal: success path returns two DPU machines for the workflow.
-	dpuMachineList := []*corev1.DpuMachine{
+	dpuMachineList := &corev1.DpuMachineList{Machines: []*corev1.DpuMachine{
 		{
 			Machine: &corev1.Machine{
 				Id:    &corev1.MachineId{Id: dpu1.ID},
@@ -3356,27 +3372,44 @@ func TestMachineHandler_GetDpuMachines(t *testing.T) {
 				DatacenterAsn:                65000,
 			},
 		},
-	}
+	}}
 
 	wrun := &tmocks.WorkflowRun{}
 	wrun.On("GetID").Return("test-workflow-id-dpu")
 	wrun.Mock.On("Get", mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
-		result := args.Get(1).(*[]*corev1.DpuMachine)
-		*result = dpuMachineList
+		result := args.Get(1).(*corev1.DpuMachineList)
+		result.Machines = dpuMachineList.Machines
 	}).Return(nil)
 
 	tsc := &tmocks.Client{}
 	tsc.Mock.On("ExecuteWorkflow", mock.Anything, mock.AnythingOfType("internal.StartWorkflowOptions"), "GetDpuMachines", mock.Anything).Return(wrun, nil)
 
+	wrunLegacy := &tmocks.WorkflowRun{}
+	wrunLegacy.On("GetID").Return("test-workflow-id-dpu-legacy")
+	wrunLegacy.Mock.On("Get", mock.Anything, mock.MatchedBy(func(result any) bool {
+		_, ok := result.(*corev1.DpuMachineList)
+		return ok
+	})).Return(fmt.Errorf("payload item 0: %w: cannot unmarshal array into DpuMachineList", tsdkConverter.ErrUnableToDecode)).Once()
+	wrunLegacy.Mock.On("Get", mock.Anything, mock.MatchedBy(func(result any) bool {
+		_, ok := result.(*[]*corev1.DpuMachine)
+		return ok
+	})).Run(func(args mock.Arguments) {
+		result := args.Get(1).(*[]*corev1.DpuMachine)
+		*result = dpuMachineList.Machines
+	}).Return(nil).Once()
+
+	tscLegacy := &tmocks.Client{}
+	tscLegacy.Mock.On("ExecuteWorkflow", mock.Anything, mock.AnythingOfType("internal.StartWorkflowOptions"), "GetDpuMachines", mock.Anything).Return(wrunLegacy, nil)
+
 	wrunErr := &tmocks.WorkflowRun{}
 	wrunErr.On("GetID").Return("test-workflow-error-id")
-	wrunErr.Mock.On("Get", mock.Anything, mock.Anything).Return(fmt.Errorf("workflow failed"))
+	wrunErr.Mock.On("Get", mock.Anything, mock.Anything).Return(fmt.Errorf("workflow failed")).Once()
 	tscErr := &tmocks.Client{}
 	tscErr.Mock.On("ExecuteWorkflow", mock.Anything, mock.AnythingOfType("internal.StartWorkflowOptions"), "GetDpuMachines", mock.Anything).Return(wrunErr, nil)
 
 	wrunTimeout := &tmocks.WorkflowRun{}
 	wrunTimeout.On("GetID").Return("test-workflow-timeout-id")
-	wrunTimeout.Mock.On("Get", mock.Anything, mock.Anything).Return(tp.NewTimeoutError(enums.TIMEOUT_TYPE_UNSPECIFIED, nil, nil))
+	wrunTimeout.Mock.On("Get", mock.Anything, mock.Anything).Return(tp.NewTimeoutError(enums.TIMEOUT_TYPE_UNSPECIFIED, nil, nil)).Once()
 	tscTimeout := &tmocks.Client{}
 	tscTimeout.Mock.On("ExecuteWorkflow", mock.Anything, mock.AnythingOfType("internal.StartWorkflowOptions"), "GetDpuMachines", mock.Anything).Return(wrunTimeout, nil)
 	tscTimeout.Mock.On("TerminateWorkflow", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
@@ -3389,6 +3422,9 @@ func TestMachineHandler_GetDpuMachines(t *testing.T) {
 
 	scpErr := sc.NewClientPool(tcfg)
 	scpErr.IDClientMap[site.ID.String()] = tscErr
+
+	scpLegacy := sc.NewClientPool(tcfg)
+	scpLegacy.IDClientMap[site.ID.String()] = tscLegacy
 
 	scpTimeout := sc.NewClientPool(tcfg)
 	scpTimeout.IDClientMap[site.ID.String()] = tscTimeout
@@ -3482,6 +3518,16 @@ func TestMachineHandler_GetDpuMachines(t *testing.T) {
 			verifyChildSpanner: true,
 		},
 		{
+			name:               "provider admin: legacy workflow result remains supported during rollout",
+			reqOrgName:         ipOrg1,
+			user:               ipu,
+			mID:                mWithDpu.ID,
+			scp:                scpLegacy,
+			expectedStatus:     http.StatusOK,
+			expectedDpuCount:   2,
+			verifyChildSpanner: true,
+		},
+		{
 			name:               "privileged tenant with account on provider can access",
 			reqOrgName:         tnOrgPriv,
 			user:               tnuPriv,
@@ -3555,6 +3601,8 @@ func TestMachineHandler_GetDpuMachines(t *testing.T) {
 	// regressions in timeout cleanup are caught instead of passing silently.
 	tsc.AssertExpectations(t)
 	wrun.AssertExpectations(t)
+	tscLegacy.AssertExpectations(t)
+	wrunLegacy.AssertExpectations(t)
 	tscErr.AssertExpectations(t)
 	wrunErr.AssertExpectations(t)
 	tscTimeout.AssertExpectations(t)

rest-api/site-workflow/pkg/activity/machine.go
@@ -159,7 +159,7 @@ func (mm *ManageMachine) DeleteMachineHealthReportOnSite(ctx context.Context, re
 }
 
 // GetDpuMachinesByIDs is an activity to retrieve DPU Machines by IDs with network configuration
-func (mm *ManageMachine) GetDpuMachinesByIDs(ctx context.Context, dpuMachineIDs []string) ([]*corev1.DpuMachine, error) {
+func (mm *ManageMachine) GetDpuMachinesByIDs(ctx context.Context, dpuMachineIDs []string) (*corev1.DpuMachineList, error) {
 	logger := log.With().Str("Activity", "GetDpuMachinesByIDs").Logger()
 
 	logger.Info().Msg("Starting activity")
@@ -218,7 +218,7 @@ func (mm *ManageMachine) GetDpuMachinesByIDs(ctx context.Context, dpuMachineIDs 
 
 	logger.Info().Int("DPU Machine Count", len(dpuMachines)).Msg("Completed activity")
 
-	return dpuMachines, nil
+	return &corev1.DpuMachineList{Machines: dpuMachines}, nil
 }
 
 // NewManageMachine returns a new ManageMachine activity

rest-api/site-workflow/pkg/workflow/machine.go
@@ -178,7 +178,7 @@ func CollectAndPublishMachineInventory(ctx workflow.Context) error {
 }
 
 // GetDpuMachines is a workflow to retrieve DPU Machines by IDs with network configuration
-func GetDpuMachines(ctx workflow.Context, dpuMachineIDs []string) ([]*corev1.DpuMachine, error) {
+func GetDpuMachines(ctx workflow.Context, dpuMachineIDs []string) (*corev1.DpuMachineList, error) {
 	logger := log.With().Str("Workflow", "GetDpuMachines").Logger()
 
 	logger.Info().Msg("Starting workflow")
@@ -202,7 +202,7 @@ func GetDpuMachines(ctx workflow.Context, dpuMachineIDs []string) ([]*corev1.Dpu
 	// Invoke GetDpuMachinesByIDs activity
 	var machineManager activity.ManageMachine
 
-	var result []*corev1.DpuMachine
+	var result corev1.DpuMachineList
 	err := workflow.ExecuteActivity(ctx, machineManager.GetDpuMachinesByIDs, dpuMachineIDs).Get(ctx, &result)
 	if err != nil {
 		logger.Error().Err(err).Str("Activity", "GetDpuMachinesByIDs").Msg("Failed to execute activity from workflow")
@@ -211,5 +211,5 @@ func GetDpuMachines(ctx workflow.Context, dpuMachineIDs []string) ([]*corev1.Dpu
 
 	logger.Info().Msg("Completing workflow")
 
-	return result, nil
+	return &result, nil
 }

rest-api/site-workflow/pkg/workflow/machine_test.go
@@ -229,7 +229,7 @@ func (s *GetDpuMachinesTestSuite) Test_GetDpuMachines_Success() {
 
 	dpuMachineIDs := []string{"dpu-machine-1", "dpu-machine-2", "dpu-machine-3"}
 
-	expectedResult := []*corev1.DpuMachine{
+	expectedResult := &corev1.DpuMachineList{Machines: []*corev1.DpuMachine{
 		{
 			Machine: &corev1.Machine{
 				Id: &corev1.MachineId{Id: "dpu-machine-1"},
@@ -237,6 +237,14 @@ func (s *GetDpuMachinesTestSuite) Test_GetDpuMachines_Success() {
 			DpuNetworkConfig: &corev1.ManagedHostNetworkConfigResponse{
 				VniDevice:    "vxlan48",
 				IsPrimaryDpu: true,
+				NetworkSecurityPolicyOverrides: []*corev1.ResolvedNetworkSecurityGroupRule{
+					{
+						Rule: &corev1.NetworkSecurityGroupRuleAttributes{
+							SourceNet:      &corev1.NetworkSecurityGroupRuleAttributes_SrcPrefix{SrcPrefix: "10.0.0.0/24"},
+							DestinationNet: &corev1.NetworkSecurityGroupRuleAttributes_DstPrefix{DstPrefix: "10.1.0.0/24"},
+						},
+					},
+				},
 			},
 		},
 		{
@@ -257,7 +265,7 @@ func (s *GetDpuMachinesTestSuite) Test_GetDpuMachines_Success() {
 				IsPrimaryDpu: false,
 			},
 		},
-	}
+	}}
 
 	// Mock GetDpuMachinesByIDs activity success
 	s.env.RegisterActivity(machineManager.GetDpuMachinesByIDs)
@@ -268,15 +276,17 @@ func (s *GetDpuMachinesTestSuite) Test_GetDpuMachines_Success() {
 	s.True(s.env.IsWorkflowCompleted())
 	s.NoError(s.env.GetWorkflowError())
 
-	var result []*corev1.DpuMachine
+	var result corev1.DpuMachineList
 	s.env.GetWorkflowResult(&result)
 
-	s.Equal(len(expectedResult), len(result))
-	for i, dpuMachine := range result {
-		s.Equal(expectedResult[i].Machine.Id.Id, dpuMachine.Machine.Id.Id)
-		s.Equal(expectedResult[i].DpuNetworkConfig.VniDevice, dpuMachine.DpuNetworkConfig.VniDevice)
-		s.Equal(expectedResult[i].DpuNetworkConfig.IsPrimaryDpu, dpuMachine.DpuNetworkConfig.IsPrimaryDpu)
+	s.Equal(len(expectedResult.Machines), len(result.Machines))
+	for i, dpuMachine := range result.Machines {
+		s.Equal(expectedResult.Machines[i].Machine.Id.Id, dpuMachine.Machine.Id.Id)
+		s.Equal(expectedResult.Machines[i].DpuNetworkConfig.VniDevice, dpuMachine.DpuNetworkConfig.VniDevice)
+		s.Equal(expectedResult.Machines[i].DpuNetworkConfig.IsPrimaryDpu, dpuMachine.DpuNetworkConfig.IsPrimaryDpu)
 	}
+	s.Equal("10.0.0.0/24", result.Machines[0].DpuNetworkConfig.NetworkSecurityPolicyOverrides[0].Rule.GetSrcPrefix())
+	s.Equal("10.1.0.0/24", result.Machines[0].DpuNetworkConfig.NetworkSecurityPolicyOverrides[0].Rule.GetDstPrefix())
 }
 
 func (s *GetDpuMachinesTestSuite) Test_GetDpuMachines_ActivityFails() {

NVIDIA/bare-metal-manager-core

Signed-off-by: Kyle Felter <kfelter@nvidia.com>

rest-api/api/pkg/api/handler/machine.go
@@ -19,6 +19,7 @@ import (
 
 	"go.opentelemetry.io/otel/attribute"
 	temporalClient "go.temporal.io/sdk/client"
+	tsdkConverter "go.temporal.io/sdk/converter"
 	tp "go.temporal.io/sdk/temporal"
 
 	"github.com/google/uuid"
@@ -2123,8 +2124,15 @@ func (gadmh GetAllDpuMachineHandler) Handle(c echo.Context) error {
 	logger.Info().Str("Workflow ID", wid).Msg("executed synchronous GetDpuMachines workflow")
 
 	// Block until the workflow has completed and returned success/error.
-	var controllerDpuMachines []*cwssaws.DpuMachine
+	var controllerDpuMachines cwssaws.DpuMachineList
 	wferr := we.Get(wfCtx, &controllerDpuMachines)
+	if errors.Is(wferr, tsdkConverter.ErrUnableToDecode) {
+		var legacyDpuMachines []*cwssaws.DpuMachine
+		wferr = we.Get(wfCtx, &legacyDpuMachines)
+		if wferr == nil {
+			controllerDpuMachines.Machines = legacyDpuMachines
+		}
+	}
 	if wferr != nil {
 		var timeoutErr *tp.TimeoutError
 		if errors.As(wferr, &timeoutErr) || wferr == context.DeadlineExceeded || wfCtx.Err() != nil {
@@ -2136,7 +2144,7 @@ func (gadmh GetAllDpuMachineHandler) Handle(c echo.Context) error {
 		return cutil.NewAPIErrorResponse(c, code, fmt.Sprintf("Failed to retrieve Machine DPU information from Site: %s", uwerr), nil)
 	}
 
-	apiDpuMachines = model.NewAPIDpuMachines(controllerDpuMachines, model.APIDpuMachineProtoContext{
+	apiDpuMachines = model.NewAPIDpuMachines(controllerDpuMachines.GetMachines(), model.APIDpuMachineProtoContext{
 		HostMachineID:            mID,
 		SiteID:                   site.ID,
 		InfrastructureProviderID: site.InfrastructureProviderID,

rest-api/api/pkg/api/handler/machine_test.go
@@ -36,6 +36,7 @@ import (
 
 	"go.temporal.io/api/enums/v1"
 	temporalClient "go.temporal.io/sdk/client"
+	tsdkConverter "go.temporal.io/sdk/converter"
 	tmocks "go.temporal.io/sdk/mocks"
 	tp "go.temporal.io/sdk/temporal"
 
@@ -3227,6 +3228,21 @@ func TestMachineHandler_Delete(t *testing.T) {
 	}
 }
 
+func TestGetDpuMachinesLegacyTemporalPayload(t *testing.T) {
+	legacyDpuMachines := []*cwssaws.DpuMachine{{Machine: &cwssaws.Machine{Id: &cwssaws.MachineId{Id: uuid.NewString()}}}}
+	dataConverter := tsdkConverter.GetDefaultDataConverter()
+	payloads, err := dataConverter.ToPayloads(legacyDpuMachines)
+	require.NoError(t, err)
+
+	var currentResult cwssaws.DpuMachineList
+	err = dataConverter.FromPayloads(payloads, &currentResult)
+	require.ErrorIs(t, err, tsdkConverter.ErrUnableToDecode)
+
+	var decodedLegacyResult []*cwssaws.DpuMachine
+	require.NoError(t, dataConverter.FromPayloads(payloads, &decodedLegacyResult))
+	require.Equal(t, legacyDpuMachines, decodedLegacyResult)
+}
+
 func TestMachineHandler_GetDpuMachines(t *testing.T) {
 	ctx := context.Background()
 	dbSession := testMachineInitDB(t)
@@ -3317,7 +3333,7 @@ func TestMachineHandler_GetDpuMachines(t *testing.T) {
 	cfg := common.GetTestConfig()
 
 	// Mock Temporal: success path returns two DPU machines for the workflow.
-	dpuMachineList := []*cwssaws.DpuMachine{
+	dpuMachineList := &cwssaws.DpuMachineList{Machines: []*cwssaws.DpuMachine{
 		{
 			Machine: &cwssaws.Machine{
 				Id:    &cwssaws.MachineId{Id: dpu1.ID},
@@ -3356,27 +3372,44 @@ func TestMachineHandler_GetDpuMachines(t *testing.T) {
 				DatacenterAsn:                65000,
 			},
 		},
-	}
+	}}
 
 	wrun := &tmocks.WorkflowRun{}
 	wrun.On("GetID").Return("test-workflow-id-dpu")
 	wrun.Mock.On("Get", mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
-		result := args.Get(1).(*[]*cwssaws.DpuMachine)
-		*result = dpuMachineList
+		result := args.Get(1).(*cwssaws.DpuMachineList)
+		result.Machines = dpuMachineList.Machines
 	}).Return(nil)
 
 	tsc := &tmocks.Client{}
 	tsc.Mock.On("ExecuteWorkflow", mock.Anything, mock.AnythingOfType("internal.StartWorkflowOptions"), "GetDpuMachines", mock.Anything).Return(wrun, nil)
 
+	wrunLegacy := &tmocks.WorkflowRun{}
+	wrunLegacy.On("GetID").Return("test-workflow-id-dpu-legacy")
+	wrunLegacy.Mock.On("Get", mock.Anything, mock.MatchedBy(func(result any) bool {
+		_, ok := result.(*cwssaws.DpuMachineList)
+		return ok
+	})).Return(fmt.Errorf("payload item 0: %w: cannot unmarshal array into DpuMachineList", tsdkConverter.ErrUnableToDecode)).Once()
+	wrunLegacy.Mock.On("Get", mock.Anything, mock.MatchedBy(func(result any) bool {
+		_, ok := result.(*[]*cwssaws.DpuMachine)
+		return ok
+	})).Run(func(args mock.Arguments) {
+		result := args.Get(1).(*[]*cwssaws.DpuMachine)
+		*result = dpuMachineList.Machines
+	}).Return(nil).Once()
+
+	tscLegacy := &tmocks.Client{}
+	tscLegacy.Mock.On("ExecuteWorkflow", mock.Anything, mock.AnythingOfType("internal.StartWorkflowOptions"), "GetDpuMachines", mock.Anything).Return(wrunLegacy, nil)
+
 	wrunErr := &tmocks.WorkflowRun{}
 	wrunErr.On("GetID").Return("test-workflow-error-id")
-	wrunErr.Mock.On("Get", mock.Anything, mock.Anything).Return(fmt.Errorf("workflow failed"))
+	wrunErr.Mock.On("Get", mock.Anything, mock.Anything).Return(fmt.Errorf("workflow failed")).Once()
 	tscErr := &tmocks.Client{}
 	tscErr.Mock.On("ExecuteWorkflow", mock.Anything, mock.AnythingOfType("internal.StartWorkflowOptions"), "GetDpuMachines", mock.Anything).Return(wrunErr, nil)
 
 	wrunTimeout := &tmocks.WorkflowRun{}
 	wrunTimeout.On("GetID").Return("test-workflow-timeout-id")
-	wrunTimeout.Mock.On("Get", mock.Anything, mock.Anything).Return(tp.NewTimeoutError(enums.TIMEOUT_TYPE_UNSPECIFIED, nil, nil))
+	wrunTimeout.Mock.On("Get", mock.Anything, mock.Anything).Return(tp.NewTimeoutError(enums.TIMEOUT_TYPE_UNSPECIFIED, nil, nil)).Once()
 	tscTimeout := &tmocks.Client{}
 	tscTimeout.Mock.On("ExecuteWorkflow", mock.Anything, mock.AnythingOfType("internal.StartWorkflowOptions"), "GetDpuMachines", mock.Anything).Return(wrunTimeout, nil)
 	tscTimeout.Mock.On("TerminateWorkflow", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
@@ -3389,6 +3422,9 @@ func TestMachineHandler_GetDpuMachines(t *testing.T) {
 
 	scpErr := sc.NewClientPool(tcfg)
 	scpErr.IDClientMap[site.ID.String()] = tscErr
+
+	scpLegacy := sc.NewClientPool(tcfg)
+	scpLegacy.IDClientMap[site.ID.String()] = tscLegacy
 
 	scpTimeout := sc.NewClientPool(tcfg)
 	scpTimeout.IDClientMap[site.ID.String()] = tscTimeout
@@ -3482,6 +3518,16 @@ func TestMachineHandler_GetDpuMachines(t *testing.T) {
 			verifyChildSpanner: true,
 		},
 		{
+			name:               "provider admin: legacy workflow result remains supported during rollout",
+			reqOrgName:         ipOrg1,
+			user:               ipu,
+			mID:                mWithDpu.ID,
+			scp:                scpLegacy,
+			expectedStatus:     http.StatusOK,
+			expectedDpuCount:   2,
+			verifyChildSpanner: true,
+		},
+		{
 			name:               "privileged tenant with account on provider can access",
 			reqOrgName:         tnOrgPriv,
 			user:               tnuPriv,
@@ -3555,6 +3601,8 @@ func TestMachineHandler_GetDpuMachines(t *testing.T) {
 	// regressions in timeout cleanup are caught instead of passing silently.
 	tsc.AssertExpectations(t)
 	wrun.AssertExpectations(t)
+	tscLegacy.AssertExpectations(t)
+	wrunLegacy.AssertExpectations(t)
 	tscErr.AssertExpectations(t)
 	wrunErr.AssertExpectations(t)
 	tscTimeout.AssertExpectations(t)

rest-api/site-workflow/pkg/activity/machine.go
@@ -153,7 +153,7 @@ func (mm *ManageMachine) DeleteMachineHealthReportOnSite(ctx context.Context, re
 }
 
 // GetDpuMachinesByIDs is an activity to retrieve DPU Machines by IDs with network configuration
-func (mm *ManageMachine) GetDpuMachinesByIDs(ctx context.Context, dpuMachineIDs []string) ([]*cwssaws.DpuMachine, error) {
+func (mm *ManageMachine) GetDpuMachinesByIDs(ctx context.Context, dpuMachineIDs []string) (*cwssaws.DpuMachineList, error) {
 	logger := log.With().Str("Activity", "GetDpuMachinesByIDs").Logger()
 
 	logger.Info().Msg("Starting activity")
@@ -212,7 +212,7 @@ func (mm *ManageMachine) GetDpuMachinesByIDs(ctx context.Context, dpuMachineIDs 
 
 	logger.Info().Int("DPU Machine Count", len(dpuMachines)).Msg("Completed activity")
 
-	return dpuMachines, nil
+	return &cwssaws.DpuMachineList{Machines: dpuMachines}, nil
 }
 
 // NewManageMachine returns a new ManageMachine activity

rest-api/site-workflow/pkg/workflow/machine.go
@@ -178,7 +178,7 @@ func CollectAndPublishMachineInventory(ctx workflow.Context) error {
 }
 
 // GetDpuMachines is a workflow to retrieve DPU Machines by IDs with network configuration
-func GetDpuMachines(ctx workflow.Context, dpuMachineIDs []string) ([]*cwssaws.DpuMachine, error) {
+func GetDpuMachines(ctx workflow.Context, dpuMachineIDs []string) (*cwssaws.DpuMachineList, error) {
 	logger := log.With().Str("Workflow", "GetDpuMachines").Logger()
 
 	logger.Info().Msg("Starting workflow")
@@ -202,7 +202,7 @@ func GetDpuMachines(ctx workflow.Context, dpuMachineIDs []string) ([]*cwssaws.Dp
 	// Invoke GetDpuMachinesByIDs activity
 	var machineManager activity.ManageMachine
 
-	var result []*cwssaws.DpuMachine
+	var result cwssaws.DpuMachineList
 	err := workflow.ExecuteActivity(ctx, machineManager.GetDpuMachinesByIDs, dpuMachineIDs).Get(ctx, &result)
 	if err != nil {
 		logger.Error().Err(err).Str("Activity", "GetDpuMachinesByIDs").Msg("Failed to execute activity from workflow")
@@ -211,5 +211,5 @@ func GetDpuMachines(ctx workflow.Context, dpuMachineIDs []string) ([]*cwssaws.Dp
 
 	logger.Info().Msg("Completing workflow")
 
-	return result, nil
+	return &result, nil
 }

rest-api/site-workflow/pkg/workflow/machine_test.go
@@ -229,7 +229,7 @@ func (s *GetDpuMachinesTestSuite) Test_GetDpuMachines_Success() {
 
 	dpuMachineIDs := []string{"dpu-machine-1", "dpu-machine-2", "dpu-machine-3"}
 
-	expectedResult := []*cwssaws.DpuMachine{
+	expectedResult := &cwssaws.DpuMachineList{Machines: []*cwssaws.DpuMachine{
 		{
 			Machine: &cwssaws.Machine{
 				Id: &cwssaws.MachineId{Id: "dpu-machine-1"},
@@ -237,6 +237,14 @@ func (s *GetDpuMachinesTestSuite) Test_GetDpuMachines_Success() {
 			DpuNetworkConfig: &cwssaws.ManagedHostNetworkConfigResponse{
 				VniDevice:    "vxlan48",
 				IsPrimaryDpu: true,
+				NetworkSecurityPolicyOverrides: []*cwssaws.ResolvedNetworkSecurityGroupRule{
+					{
+						Rule: &cwssaws.NetworkSecurityGroupRuleAttributes{
+							SourceNet:      &cwssaws.NetworkSecurityGroupRuleAttributes_SrcPrefix{SrcPrefix: "10.0.0.0/24"},
+							DestinationNet: &cwssaws.NetworkSecurityGroupRuleAttributes_DstPrefix{DstPrefix: "10.1.0.0/24"},
+						},
+					},
+				},
 			},
 		},
 		{
@@ -257,7 +265,7 @@ func (s *GetDpuMachinesTestSuite) Test_GetDpuMachines_Success() {
 				IsPrimaryDpu: false,
 			},
 		},
-	}
+	}}
 
 	// Mock GetDpuMachinesByIDs activity success
 	s.env.RegisterActivity(machineManager.GetDpuMachinesByIDs)
@@ -268,15 +276,17 @@ func (s *GetDpuMachinesTestSuite) Test_GetDpuMachines_Success() {
 	s.True(s.env.IsWorkflowCompleted())
 	s.NoError(s.env.GetWorkflowError())
 
-	var result []*cwssaws.DpuMachine
+	var result cwssaws.DpuMachineList
 	s.env.GetWorkflowResult(&result)
 
-	s.Equal(len(expectedResult), len(result))
-	for i, dpuMachine := range result {
-		s.Equal(expectedResult[i].Machine.Id.Id, dpuMachine.Machine.Id.Id)
-		s.Equal(expectedResult[i].DpuNetworkConfig.VniDevice, dpuMachine.DpuNetworkConfig.VniDevice)
-		s.Equal(expectedResult[i].DpuNetworkConfig.IsPrimaryDpu, dpuMachine.DpuNetworkConfig.IsPrimaryDpu)
+	s.Equal(len(expectedResult.Machines), len(result.Machines))
+	for i, dpuMachine := range result.Machines {
+		s.Equal(expectedResult.Machines[i].Machine.Id.Id, dpuMachine.Machine.Id.Id)
+		s.Equal(expectedResult.Machines[i].DpuNetworkConfig.VniDevice, dpuMachine.DpuNetworkConfig.VniDevice)
+		s.Equal(expectedResult.Machines[i].DpuNetworkConfig.IsPrimaryDpu, dpuMachine.DpuNetworkConfig.IsPrimaryDpu)
 	}
+	s.Equal("10.0.0.0/24", result.Machines[0].DpuNetworkConfig.NetworkSecurityPolicyOverrides[0].Rule.GetSrcPrefix())
+	s.Equal("10.1.0.0/24", result.Machines[0].DpuNetworkConfig.NetworkSecurityPolicyOverrides[0].Rule.GetDstPrefix())
 }
 
 func (s *GetDpuMachinesTestSuite) Test_GetDpuMachines_ActivityFails() {