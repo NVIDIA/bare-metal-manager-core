roehrijn/infra-controller

feat(bmc-mock): add libvirt-backed virtual media
Signed-off-by: Brennen Murray <brennenm@nvidia.com>

crates/bmc-mock/src/main.rs
@@ -17,7 +17,8 @@
 mod command_line;
 mod tar_router;
 
-use std::collections::HashMap;
+use std::borrow::Cow;
+use std::collections::{BTreeMap, HashMap};
 use std::io::ErrorKind;
 use std::net::SocketAddr;
 use std::process::Command;
@@ -30,9 +31,10 @@ use bmc_mock::mac_address_pool::{
 };
 use bmc_mock::{
     BmcCommand, BmcState, Callbacks, DpuMachineInfo, DpuSettings, HostHardwareType,
-    HostMachineInfo, ListenerOrAddress, MachineInfo, MockPowerState, SetSystemPowerError,
-    SystemPowerControl,
+    HostMachineInfo, ListenerOrAddress, MachineInfo, MachineRouterOptions, MockPowerState,
+    SetSystemPowerError, SystemPowerControl, VirtualMediaDeviceConfig,
 };
+use command_line::{MachineRole, StateBackend};
 use mac_address::MacAddress;
 use tar_router::TarGzOption;
 use tokio::sync::{RwLock, mpsc};
@@ -76,6 +78,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             "--enable-ipmi-simulation cannot be combined with archive-backed routers".into(),
         );
     }
+    let has_generated_options = args.libvirt_domain.is_some()
+        || args.hardware_profile.is_some()
+        || args.machine_role.is_some()
+        || args.state_backend.is_some()
+        || args.dpu_count.is_some()
+        || args.dpu_index.is_some()
+        || args.instance_index != 0;
+    if has_generated_options && (args.targz.is_some() || args.ip_router.is_some()) {
+        return Err(
+            "generated-machine options cannot be combined with archive-backed routers".into(),
+        );
+    }
+    let generated_config = generated_mock_config(&args)?;
     if let Some(ip_routers) = args.ip_router {
         for ip_router in ip_routers {
             info!(
@@ -102,8 +117,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             None,
         )
     } else {
-        info!("Using default BMC mock");
-        let (router, state) = default_host_mock();
+        info!(
+            hardware_type = %generated_config.hardware_type,
+            role = ?generated_config.machine_role,
+            backend = ?generated_config.state_backend,
+            "Using generated BMC mock",
+        );
+        let (router, state) = generated_mock(generated_config);
         (router, Some(state))
     };
 
@@ -195,34 +215,208 @@ fn spawn_qemu_reboot_handler() -> mpsc::UnboundedSender<BmcCommand> {
     command_tx
 }
 
-fn default_host_mock() -> (Router, BmcState) {
-    let command_channel = spawn_qemu_reboot_handler();
-    let callbacks = Arc::new(ChannelCallbacks::new(command_channel));
+#[derive(Debug)]
+struct GeneratedMockConfig {
+    machine_role: MachineRole,
+    state_backend: StateBackend,
+    hardware_type: HostHardwareType,
+    dpu_count: u8,
+    dpu_index: usize,
+    instance_index: u8,
+    libvirt_config: Option<bmc_mock::libvirt::Config>,
+    legacy_callbacks: bool,
+}
+
+fn generated_mock_config(args: &command_line::Args) -> Result<GeneratedMockConfig, String> {
+    let explicit_mode = args.machine_role.is_some() || args.state_backend.is_some();
+    let compatibility_libvirt_mode =
+        args.libvirt_domain.is_some() && args.hardware_profile.is_some() && !explicit_mode;
+    let legacy_mode = args.libvirt_domain.is_none()
+        && args.hardware_profile.is_none()
+        && !explicit_mode
+        && args.dpu_count.is_none()
+        && args.dpu_index.is_none()
+        && args.instance_index == 0;
+
+    if legacy_mode {
+        return Ok(GeneratedMockConfig {
+            machine_role: MachineRole::Host,
+            state_backend: StateBackend::Internal,
+            hardware_type: HostHardwareType::WiwynnGB200Nvl,
+            dpu_count: 2,
+            dpu_index: 0,
+            instance_index: 0,
+            libvirt_config: None,
+            legacy_callbacks: true,
+        });
+    }
+
+    if !explicit_mode && !compatibility_libvirt_mode {
+        return Err(
+            "explicit generated machines require --hardware-profile, --machine-role, and --state-backend"
+                .to_string(),
+        );
+    }
+
+    let machine_role = if compatibility_libvirt_mode {
+        MachineRole::Host
+    } else {
+        args.machine_role.ok_or_else(|| {
+            "--machine-role is required when explicitly configuring a generated machine".to_string()
+        })?
+    };
+    let state_backend = if compatibility_libvirt_mode {
+        StateBackend::Libvirt
+    } else {
+        args.state_backend.ok_or_else(|| {
+            "--state-backend is required when explicitly configuring a generated machine"
+                .to_string()
+        })?
+    };
+    let hardware_type = args.hardware_profile.ok_or_else(|| {
+        "--hardware-profile is required when explicitly configuring a generated machine".to_string()
+    })?;
+
+    let dpu_count = match (hardware_type.fixed_number_of_dpu(), args.dpu_count) {
+        (Some(fixed), Some(requested)) if fixed != requested => {
+            return Err(format!(
+                "hardware profile {hardware_type} requires {fixed} DPU(s), not {requested}"
+            ));
+        }
+        (Some(fixed), _) => fixed,
+        (None, requested) => requested.unwrap_or(0),
+    };
+    let dpu_index = args.dpu_index.unwrap_or(0);
+    match machine_role {
+        MachineRole::Host if args.dpu_index.is_some() => {
+            return Err("--dpu-index is valid only with --machine-role=dpu".to_string());
+        }
+        MachineRole::Dpu if dpu_index >= usize::from(dpu_count) => {
+            return Err(format!(
+                "DPU index {dpu_index} is outside hardware profile {hardware_type}'s {dpu_count} DPU(s)"
+            ));
+        }
+        _ => {}
+    }
+
+    let libvirt_config = match (state_backend, &args.libvirt_domain) {
+        (StateBackend::Libvirt, Some(domain)) => Some(bmc_mock::libvirt::Config {
+            virsh_path: args.virsh_path.clone(),
+            uri: args.libvirt_uri.clone(),
+            domain: domain.clone(),
+            virtual_media_targets: BTreeMap::from([
+                ("Cd".to_string(), "sdb".to_string()),
+                ("ConfigCd".to_string(), "sdc".to_string()),
+            ]),
+        }),
+        (StateBackend::Libvirt, None) => {
+            return Err("--state-backend=libvirt requires --libvirt-domain".to_string());
+        }
+        (StateBackend::Internal, Some(_)) => {
+            return Err("--libvirt-domain requires --state-backend=libvirt".to_string());
+        }
+        (StateBackend::Internal, None) => None,
+    };
+
+    Ok(GeneratedMockConfig {
+        machine_role,
+        state_backend,
+        hardware_type,
+        dpu_count,
+        dpu_index,
+        instance_index: args.instance_index,
+        libvirt_config,
+        legacy_callbacks: false,
+    })
+}
+
+fn generated_mock(config: GeneratedMockConfig) -> (Router, BmcState) {
+    let libvirt_callbacks = config
+        .libvirt_config
+        .map(bmc_mock::libvirt::LibvirtCallbacks::new)
+        .map(Arc::new);
+    let callbacks: Arc<dyn Callbacks> = if config.legacy_callbacks {
+        let command_channel = spawn_qemu_reboot_handler();
+        Arc::new(ChannelCallbacks::new(command_channel))
+    } else if let Some(callbacks) = &libvirt_callbacks {
+        callbacks.clone()
+    } else {
+        Arc::new(bmc_mock::simulated::SimulatedCallbacks::new())
+    };
+    let machine_info = generated_machine_info(
+        config.machine_role,
+        config.hardware_type,
+        config.dpu_count,
+        config.dpu_index,
+        config.instance_index,
+    );
+    let result = if config.machine_role == MachineRole::Host
+        && config.state_backend == StateBackend::Libvirt
+    {
+        bmc_mock::machine_router_with_options(
+            &machine_info,
+            callbacks,
+            String::default(),
+            false,
+            MachineRouterOptions {
+                virtual_media_devices: vec![
+                    VirtualMediaDeviceConfig {
+                        id: Cow::Borrowed("Cd"),
+                        name: Cow::Borrowed("Operating System Virtual CD"),
+                        media_types: vec![Cow::Borrowed("CD"), Cow::Borrowed("DVD")],
+                    },
+                    VirtualMediaDeviceConfig {
+                        id: Cow::Borrowed("ConfigCd"),
+                        name: Cow::Borrowed("Configuration Virtual CD"),
+                        media_types: vec![Cow::Borrowed("CD"), Cow::Borrowed("DVD")],
+                    },
+                ],
+            },
+        )
+    } else {
+        bmc_mock::machine_router(&machine_info, callbacks, String::default(), false)
+    };
+    if let Some(callbacks) = libvirt_callbacks {
+        callbacks
+            .bind_state(&result.1)
+            .expect("libvirt backend must bind to generated BMC state");
+    }
+    result
+}
+
+fn generated_machine_info(
+    machine_role: MachineRole,
+    hardware_type: HostHardwareType,
+    dpu_count: u8,
+    dpu_index: usize,
+    instance_index: u8,
+) -> MachineInfo {
     let mut pool = MacAddressPool::new(MacAddressConfig {
         pool: Some(
-            MacAddressPoolConfig::new(MacAddress::new([2, 0, 0, 0, 0, 0]), 16)
+            MacAddressPoolConfig::new(MacAddress::new([2, 0, 0, instance_index, 0, 0]), 16)
                 .expect("Must be constructed with these parameters"),
         ),
         ranges: Some(
-            MacAddressRangesConfig::new(MacAddress::new([6, 0, 0, 0, 0, 0]), 16, 8)
+            MacAddressRangesConfig::new(MacAddress::new([6, 0, 0, instance_index, 0, 0]), 16, 8)
                 .expect("Must be constructed with these parameters"),
         ),
     });
 
-    let hw_type = HostHardwareType::WiwynnGB200Nvl;
-    let ndpu = hw_type.fixed_number_of_dpu().unwrap_or(1);
     let mac_range = pool
         .allocate_range_config()
         .expect("MAC address pool should be allocated");
-    let machine_info = MachineInfo::Host(HostMachineInfo::new(
-        HostHardwareType::WiwynnGB200Nvl,
-        (0..ndpu)
-            .map(|_| DpuMachineInfo::new(hw_type, &mut pool, DpuSettings::default()))
-            .collect(),
-        &mut pool,
-        mac_range,
-    ));
-    bmc_mock::machine_router(&machine_info, callbacks, String::default(), false)
+    let dpus = (0..dpu_count)
+        .map(|_| DpuMachineInfo::new(hardware_type, &mut pool, DpuSettings::default()))
+        .collect::<Vec<_>>();
+    match machine_role {
+        MachineRole::Host => MachineInfo::Host(HostMachineInfo::new(
+            hardware_type,
+            dpus,
+            &mut pool,
+            mac_range,
+        )),
+        MachineRole::Dpu => MachineInfo::Dpu(dpus[dpu_index].clone()),
+    }
 }
 
 #[derive(Debug)]
@@ -257,5 +451,97 @@ impl Callbacks for ChannelCallbacks {
         let _ = self
             .command_channel
             .send(BmcCommand::StateRefreshIndication);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use clap::Parser;
+
+    use super::*;
+
+    fn config(arguments: &[&str]) -> Result<GeneratedMockConfig, String> {
+        let args = command_line::Args::try_parse_from(arguments).unwrap();
+        generated_mock_config(&args)
+    }
+
+    #[test]
+    fn preserves_the_pre_libvirt_default_mode() {
+        let config = config(&["bmc-mock"]).unwrap();
+        assert_eq!(config.machine_role, MachineRole::Host);
+        assert_eq!(config.state_backend, StateBackend::Internal);
+        assert!(matches!(
+            config.hardware_type,
+            HostHardwareType::WiwynnGB200Nvl
+        ));
+        assert!(config.legacy_callbacks);
+    }
+
+    #[test]
+    fn preserves_the_existing_libvirt_shorthand() {
+        let config = config(&[
+            "bmc-mock",
+            "--libvirt-domain",
+            "host-01",
+            "--hardware-profile",
+            "dell_poweredge_r750",
+        ])
+        .unwrap();
+        assert_eq!(config.machine_role, MachineRole::Host);
+        assert_eq!(config.state_backend, StateBackend::Libvirt);
+        assert!(!config.legacy_callbacks);
+    }
+
+    #[test]
+    fn validates_dpu_count_and_index() {
+        let cases = [
+            [
+                "bmc-mock",
+                "--machine-role",
+                "dpu",
+                "--state-backend",
+                "internal",
+                "--hardware-profile",
+                "wiwynn_gb200_nvl",
+                "--dpu-count",
+                "1",
+                "--dpu-index",
+                "0",
+            ],
+            [
+                "bmc-mock",
+                "--machine-role",
+                "dpu",
+                "--state-backend",
+                "internal",
+                "--hardware-profile",
+                "wiwynn_gb200_nvl",
+                "--dpu-count",
+                "2",
+                "--dpu-index",
+                "2",
+            ],
+        ];
+
+        for arguments in cases {
+            assert!(config(&arguments).is_err(), "arguments {arguments:?}");
+        }
+    }
+
+    #[test]
+    fn host_and_dpu_endpoints_share_deterministic_dpu_identity() {
+        let host =
+            generated_machine_info(MachineRole::Host, HostHardwareType::WiwynnGB200Nvl, 2, 0, 3);
+        let dpu =
+            generated_machine_info(MachineRole::Dpu, HostHardwareType::WiwynnGB200Nvl, 2, 1, 3);
+        let MachineInfo::Host(host) = host else {
+            panic!("expected host machine info");
+        };
+        let MachineInfo::Dpu(dpu) = dpu else {
+            panic!("expected DPU machine info");
+        };
+
+        assert_eq!(host.dpus[1].serial, dpu.serial);
+        assert_eq!(host.dpus[1].bmc_mac_address, dpu.bmc_mac_address);
     }
 }

NVIDIA/bare-metal-manager-core

crates/bmc-mock/src/main.rs
@@ -17,7 +17,8 @@
 mod command_line;
 mod tar_router;
 
-use std::collections::HashMap;
+use std::borrow::Cow;
+use std::collections::{BTreeMap, HashMap};
 use std::io::ErrorKind;
 use std::net::SocketAddr;
 use std::process::Command;
@@ -30,9 +31,10 @@ use bmc_mock::mac_address_pool::{
 };
 use bmc_mock::{
     BmcCommand, BmcState, Callbacks, DpuMachineInfo, DpuSettings, HostHardwareType,
-    HostMachineInfo, ListenerOrAddress, MachineInfo, MockPowerState, SetSystemPowerError,
-    SystemPowerControl,
+    HostMachineInfo, ListenerOrAddress, MachineInfo, MachineRouterOptions, MockPowerState,
+    SetSystemPowerError, SystemPowerControl, VirtualMediaDeviceConfig,
 };
+use command_line::{MachineRole, StateBackend};
 use mac_address::MacAddress;
 use tar_router::TarGzOption;
 use tokio::sync::{RwLock, mpsc};
@@ -76,6 +78,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             "--enable-ipmi-simulation cannot be combined with archive-backed routers".into(),
         );
     }
+    let has_generated_options = args.libvirt_domain.is_some()
+        || args.hardware_profile.is_some()
+        || args.machine_role.is_some()
+        || args.state_backend.is_some()
+        || args.dpu_count.is_some()
+        || args.dpu_index.is_some()
+        || args.instance_index != 0;
+    if has_generated_options && (args.targz.is_some() || args.ip_router.is_some()) {
+        return Err(
+            "generated-machine options cannot be combined with archive-backed routers".into(),
+        );
+    }
+    let generated_config = generated_mock_config(&args)?;
     if let Some(ip_routers) = args.ip_router {
         for ip_router in ip_routers {
             info!(
@@ -102,8 +117,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             None,
         )
     } else {
-        info!("Using default BMC mock");
-        let (router, state) = default_host_mock();
+        info!(
+            hardware_type = %generated_config.hardware_type,
+            role = ?generated_config.machine_role,
+            backend = ?generated_config.state_backend,
+            "Using generated BMC mock",
+        );
+        let (router, state) = generated_mock(generated_config);
         (router, Some(state))
     };
 
@@ -195,34 +215,214 @@ fn spawn_qemu_reboot_handler() -> mpsc::UnboundedSender<BmcCommand> {
     command_tx
 }
 
-fn default_host_mock() -> (Router, BmcState) {
-    let command_channel = spawn_qemu_reboot_handler();
-    let callbacks = Arc::new(ChannelCallbacks::new(command_channel));
+#[derive(Debug)]
+struct GeneratedMockConfig {
+    machine_role: MachineRole,
+    state_backend: StateBackend,
+    hardware_type: HostHardwareType,
+    dpu_count: u8,
+    dpu_index: usize,
+    instance_index: u8,
+    libvirt_config: Option<bmc_mock::libvirt::Config>,
+    use_channel_callbacks: bool,
+}
+
+fn generated_mock_config(args: &command_line::Args) -> Result<GeneratedMockConfig, String> {
+    let explicit_mode = args.machine_role.is_some() || args.state_backend.is_some();
+    let libvirt_shorthand_mode =
+        args.libvirt_domain.is_some() && args.hardware_profile.is_some() && !explicit_mode;
+    let default_generated_mode = args.libvirt_domain.is_none()
+        && args.hardware_profile.is_none()
+        && !explicit_mode
+        && args.dpu_count.is_none()
+        && args.dpu_index.is_none()
+        && args.instance_index == 0;
+
+    if default_generated_mode {
+        return Ok(GeneratedMockConfig {
+            machine_role: MachineRole::Host,
+            state_backend: StateBackend::Internal,
+            hardware_type: HostHardwareType::WiwynnGB200Nvl,
+            dpu_count: 2,
+            dpu_index: 0,
+            instance_index: 0,
+            libvirt_config: None,
+            use_channel_callbacks: true,
+        });
+    }
+
+    if !explicit_mode && !libvirt_shorthand_mode {
+        return Err(
+            "explicit generated machines require --hardware-profile, --machine-role, and --state-backend"
+                .to_string(),
+        );
+    }
+
+    let machine_role = if libvirt_shorthand_mode {
+        MachineRole::Host
+    } else {
+        args.machine_role.ok_or_else(|| {
+            "--machine-role is required when explicitly configuring a generated machine".to_string()
+        })?
+    };
+    let state_backend = if libvirt_shorthand_mode {
+        StateBackend::Libvirt
+    } else {
+        args.state_backend.ok_or_else(|| {
+            "--state-backend is required when explicitly configuring a generated machine"
+                .to_string()
+        })?
+    };
+    let hardware_type = args.hardware_profile.ok_or_else(|| {
+        "--hardware-profile is required when explicitly configuring a generated machine".to_string()
+    })?;
+
+    let dpu_count = match (hardware_type.fixed_number_of_dpu(), args.dpu_count) {
+        (Some(fixed), Some(requested)) if fixed != requested => {
+            return Err(format!(
+                "hardware profile {hardware_type} requires {fixed} DPU(s), not {requested}"
+            ));
+        }
+        (Some(fixed), _) => fixed,
+        (None, requested) => requested.unwrap_or(0),
+    };
+    let dpu_index = args.dpu_index.unwrap_or(0);
+    match machine_role {
+        MachineRole::Host if args.dpu_index.is_some() => {
+            return Err("--dpu-index is valid only with --machine-role=dpu".to_string());
+        }
+        MachineRole::Dpu if dpu_index >= usize::from(dpu_count) => {
+            return Err(format!(
+                "DPU index {dpu_index} is outside hardware profile {hardware_type}'s {dpu_count} DPU(s)"
+            ));
+        }
+        _ => {}
+    }
+
+    let libvirt_config = match (state_backend, &args.libvirt_domain) {
+        (StateBackend::Libvirt, Some(domain)) => Some(bmc_mock::libvirt::Config {
+            virsh_path: args.virsh_path.clone(),
+            uri: args.libvirt_uri.clone(),
+            domain: domain.clone(),
+            virtual_media_targets: BTreeMap::from([
+                ("Cd".to_string(), "sdb".to_string()),
+                ("ConfigCd".to_string(), "sdc".to_string()),
+            ]),
+        }),
+        (StateBackend::Libvirt, None) => {
+            return Err("--state-backend=libvirt requires --libvirt-domain".to_string());
+        }
+        (StateBackend::Internal, Some(_)) => {
+            return Err("--libvirt-domain requires --state-backend=libvirt".to_string());
+        }
+        (StateBackend::Internal, None) => None,
+    };
+
+    Ok(GeneratedMockConfig {
+        machine_role,
+        state_backend,
+        hardware_type,
+        dpu_count,
+        dpu_index,
+        instance_index: args.instance_index,
+        libvirt_config,
+        use_channel_callbacks: false,
+    })
+}
+
+fn generated_mock(config: GeneratedMockConfig) -> (Router, BmcState) {
+    let libvirt_callbacks = config
+        .libvirt_config
+        .map(bmc_mock::libvirt::LibvirtCallbacks::new)
+        .map(Arc::new);
+    let callbacks: Arc<dyn Callbacks> = if config.use_channel_callbacks {
+        let command_channel = spawn_qemu_reboot_handler();
+        Arc::new(ChannelCallbacks::new(command_channel))
+    } else if let Some(callbacks) = &libvirt_callbacks {
+        callbacks.clone()
+    } else {
+        Arc::new(bmc_mock::simulated::SimulatedCallbacks::new())
+    };
+    let machine_info = generated_machine_info(
+        config.machine_role,
+        config.hardware_type,
+        config.dpu_count,
+        config.dpu_index,
+        config.instance_index,
+    );
+    let result = if config.machine_role == MachineRole::Host
+        && config.state_backend == StateBackend::Libvirt
+    {
+        bmc_mock::machine_router(
+            &machine_info,
+            callbacks,
+            String::default(),
+            false,
+            MachineRouterOptions {
+                virtual_media_devices: Some(vec![
+                    VirtualMediaDeviceConfig {
+                        id: Cow::Borrowed("Cd"),
+                        name: Cow::Borrowed("Operating System Virtual CD"),
+                        media_types: vec![Cow::Borrowed("CD"), Cow::Borrowed("DVD")],
+                    },
+                    VirtualMediaDeviceConfig {
+                        id: Cow::Borrowed("ConfigCd"),
+                        name: Cow::Borrowed("Configuration Virtual CD"),
+                        media_types: vec![Cow::Borrowed("CD"), Cow::Borrowed("DVD")],
+                    },
+                ]),
+            },
+        )
+    } else {
+        bmc_mock::machine_router(
+            &machine_info,
+            callbacks,
+            String::default(),
+            false,
+            MachineRouterOptions::default(),
+        )
+    };
+    if let Some(callbacks) = libvirt_callbacks {
+        callbacks
+            .bind_state(&result.1)
+            .expect("libvirt backend must bind to generated BMC state");
+    }
+    result
+}
+
+fn generated_machine_info(
+    machine_role: MachineRole,
+    hardware_type: HostHardwareType,
+    dpu_count: u8,
+    dpu_index: usize,
+    instance_index: u8,
+) -> MachineInfo {
     let mut pool = MacAddressPool::new(MacAddressConfig {
         pool: Some(
-            MacAddressPoolConfig::new(MacAddress::new([2, 0, 0, 0, 0, 0]), 16)
+            MacAddressPoolConfig::new(MacAddress::new([2, 0, 0, instance_index, 0, 0]), 16)
                 .expect("Must be constructed with these parameters"),
         ),
         ranges: Some(
-            MacAddressRangesConfig::new(MacAddress::new([6, 0, 0, 0, 0, 0]), 16, 8)
+            MacAddressRangesConfig::new(MacAddress::new([6, 0, 0, instance_index, 0, 0]), 16, 8)
                 .expect("Must be constructed with these parameters"),
         ),
     });
 
-    let hw_type = HostHardwareType::WiwynnGB200Nvl;
-    let ndpu = hw_type.fixed_number_of_dpu().unwrap_or(1);
     let mac_range = pool
         .allocate_range_config()
         .expect("MAC address pool should be allocated");
-    let machine_info = MachineInfo::Host(HostMachineInfo::new(
-        HostHardwareType::WiwynnGB200Nvl,
-        (0..ndpu)
-            .map(|_| DpuMachineInfo::new(hw_type, &mut pool, DpuSettings::default()))
-            .collect(),
-        &mut pool,
-        mac_range,
-    ));
-    bmc_mock::machine_router(&machine_info, callbacks, String::default(), false)
+    let dpus = (0..dpu_count)
+        .map(|_| DpuMachineInfo::new(hardware_type, &mut pool, DpuSettings::default()))
+        .collect::<Vec<_>>();
+    match machine_role {
+        MachineRole::Host => MachineInfo::Host(HostMachineInfo::new(
+            hardware_type,
+            dpus,
+            &mut pool,
+            mac_range,
+        )),
+        MachineRole::Dpu => MachineInfo::Dpu(dpus[dpu_index].clone()),
+    }
 }
 
 #[derive(Debug)]
@@ -257,5 +457,97 @@ impl Callbacks for ChannelCallbacks {
         let _ = self
             .command_channel
             .send(BmcCommand::StateRefreshIndication);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use clap::Parser;
+
+    use super::*;
+
+    fn config(arguments: &[&str]) -> Result<GeneratedMockConfig, String> {
+        let args = command_line::Args::try_parse_from(arguments).unwrap();
+        generated_mock_config(&args)
+    }
+
+    #[test]
+    fn default_generated_mode_uses_channel_callbacks() {
+        let config = config(&["bmc-mock"]).unwrap();
+        assert_eq!(config.machine_role, MachineRole::Host);
+        assert_eq!(config.state_backend, StateBackend::Internal);
+        assert!(matches!(
+            config.hardware_type,
+            HostHardwareType::WiwynnGB200Nvl
+        ));
+        assert!(config.use_channel_callbacks);
+    }
+
+    #[test]
+    fn preserves_the_existing_libvirt_shorthand() {
+        let config = config(&[
+            "bmc-mock",
+            "--libvirt-domain",
+            "host-01",
+            "--hardware-profile",
+            "dell_poweredge_r750",
+        ])
+        .unwrap();
+        assert_eq!(config.machine_role, MachineRole::Host);
+        assert_eq!(config.state_backend, StateBackend::Libvirt);
+        assert!(!config.use_channel_callbacks);
+    }
+
+    #[test]
+    fn validates_dpu_count_and_index() {
+        let cases = [
+            [
+                "bmc-mock",
+                "--machine-role",
+                "dpu",
+                "--state-backend",
+                "internal",
+                "--hardware-profile",
+                "wiwynn_gb200_nvl",
+                "--dpu-count",
+                "1",
+                "--dpu-index",
+                "0",
+            ],
+            [
+                "bmc-mock",
+                "--machine-role",
+                "dpu",
+                "--state-backend",
+                "internal",
+                "--hardware-profile",
+                "wiwynn_gb200_nvl",
+                "--dpu-count",
+                "2",
+                "--dpu-index",
+                "2",
+            ],
+        ];
+
+        for arguments in cases {
+            assert!(config(&arguments).is_err(), "arguments {arguments:?}");
+        }
+    }
+
+    #[test]
+    fn host_and_dpu_endpoints_share_deterministic_dpu_identity() {
+        let host =
+            generated_machine_info(MachineRole::Host, HostHardwareType::WiwynnGB200Nvl, 2, 0, 3);
+        let dpu =
+            generated_machine_info(MachineRole::Dpu, HostHardwareType::WiwynnGB200Nvl, 2, 1, 3);
+        let MachineInfo::Host(host) = host else {
+            panic!("expected host machine info");
+        };
+        let MachineInfo::Dpu(dpu) = dpu else {
+            panic!("expected DPU machine info");
+        };
+
+        assert_eq!(host.dpus[1].serial, dpu.serial);
+        assert_eq!(host.dpus[1].bmc_mac_address, dpu.bmc_mac_address);
     }
 }