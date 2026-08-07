fdc18381942b491ec581390867e294509444a93c
Drew Bloechl <drew@nvidia.com>
2026-08-07 11:00:22 -0700

    Move types module into its own directory

---
+++ crates/nvue-client/src/types/mod.rs
@@ -0,0 +1,8 @@
+#[derive(Debug, serde::Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct MacTableEntry {
+    pub mac: String,
+    pub interface: String,
+    pub entry_type: String,
+    pub vlan: Option<u16>,
+}
