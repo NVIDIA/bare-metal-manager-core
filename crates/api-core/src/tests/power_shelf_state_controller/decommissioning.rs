8287af39f566d891dabea5f0a89c4d410de28d07
Eric Wetzel <ewetzel@nvidia.com>
2026-08-08 06:05:20 +0000

    fix test error

--- crates/api-core/src/tests/power_shelf_state_controller/decommissioning.rs
+++ crates/api-core/src/tests/power_shelf_state_controller/decommissioning.rs
@@ -15,87 +15,19 @@
  * limitations under the License.
  */
 
-use std::sync::Arc;
-use std::time::Duration;
-
-use carbide_credential_rotation::RotationGate;
-use carbide_power_shelf_controller::context::PowerShelfStateHandlerServices;
-use carbide_power_shelf_controller::handler::PowerShelfStateHandler;
-use carbide_power_shelf_controller::io::PowerShelfStateControllerIO;
-use carbide_uuid::machine::{MachineId, MachineIdSource, MachineInterfaceId, MachineType};
-use carbide_uuid::network::NetworkSegmentId;
+use carbide_uuid::machine::{MachineId, MachineIdSource, MachineType};
 use carbide_uuid::power_shelf::PowerShelfId;
 use carbide_uuid::rack::RackId;
-use mac_address::MacAddress;
-use model::allocation_type::AllocationType;
-use model::bmc_suppression::BmcSuppressionSubsystem;
-use model::power_shelf::{
-    PowerShelf, PowerShelfControllerState, PowerShelfDecommissioningState,
-    PowerShelfVerifyingDhcpReleaseState,
-};
+use model::power_shelf::{PowerShelf, PowerShelfControllerState};
 use rpc::forge::forge_server::Forge;
-use state_controller::config::IterationConfig;
-use state_controller::controller::StateController;
-use tokio_util::sync::CancellationToken;
 use tonic::{Code, Request};
 
 use super::fixtures::power_shelf::set_power_shelf_controller_state;
 use crate::tests::common;
 use crate::tests::common::api_fixtures::site_explorer::TestRackDbBuilder;
-use crate::tests::common::api_fixtures::{TestEnv, create_test_env};
+use crate::tests::common::api_fixtures::create_test_env;
 
 type TestResult<T = ()> = Result<T, Box<dyn std::error::Error>>;
-
-fn services(env: &TestEnv, pool: &sqlx::PgPool) -> PowerShelfStateHandlerServices {
-    PowerShelfStateHandlerServices {
-        db_pool: pool.clone(),
-        component_manager: None,
-        credential_manager: env.test_credential_manager.clone(),
-        per_object_metrics_registry: env.per_object_metrics_registry(),
-        rack_firmware_reprovisioning_enabled: false,
-        redfish_client_pool: env.redfish_sim.clone(),
-        bmc_rotation_gate: RotationGate::new_for_family(
-            db::credential_rotation::CredentialRotationType::Bmc,
-        ),
-        bmc_rotation_enabled: false,
-    }
-}
-
-async fn seed_pmc_endpoint(
-    pool: &sqlx::PgPool,
-    power_shelf_id: PowerShelfId,
-) -> TestResult<MacAddress> {
-    let mut txn = pool.begin().await?;
-    let segment_id: NetworkSegmentId = sqlx::query_scalar(
-        "INSERT INTO network_segments (name, version, network_segment_type)
-         VALUES ($1, 'V1-T0', 'tenant') RETURNING id",
-    )
-    .bind(format!("decommission-pmc-{power_shelf_id}"))
-    .fetch_one(txn.as_mut())
-    .await?;
-    let pmc_mac: MacAddress = "02:00:00:00:0d:01".parse()?;
-    let interface_id: MachineInterfaceId = sqlx::query_scalar(
-        "INSERT INTO machine_interfaces
-             (power_shelf_id, association_type, segment_id, mac_address,
-              primary_interface, hostname, interface_type)
-         VALUES ($1, 'PowerShelf', $2, $3, false, 'pmc', 'Bmc')
-         RETURNING id",
-    )
-    .bind(power_shelf_id)
-    .bind(segment_id)
-    .bind(pmc_mac)
-    .fetch_one(txn.as_mut())
-    .await?;
-    db::machine_interface_address::insert(
-        txn.as_mut(),
-        interface_id,
-        "10.30.40.60".parse()?,
-        AllocationType::Dhcp,
-    )
-    .await?;
-    txn.commit().await?;
-    Ok(pmc_mac)
-}
 
 async fn load_power_shelf(pool: &sqlx::PgPool, id: PowerShelfId) -> TestResult<PowerShelf> {
     let mut conn = pool.acquire().await?;
@@ -203,166 +135,6 @@ async fn decommission_rejects_instance_assigned_host_in_power_shelf_rack(
         load_power_shelf(&pool, power_shelf_id)
             .await?
             .decommission_requested
-    );
-    Ok(())
-}
-
-#[crate::sqlx_test]
-async fn managed_power_shelf_reaches_decommissioned_after_dhcp_release(
-    pool: sqlx::PgPool,
-) -> TestResult {
-    let env = create_test_env(pool.clone()).await;
-    let power_shelf_id = common::api_fixtures::site_explorer::new_power_shelf(
-        &env,
-        Some("Decommission workflow".to_string()),
-        None,
-        None,
-        None,
-    )
-    .await?;
-    let pmc_mac = seed_pmc_endpoint(&pool, power_shelf_id).await?;
-    let mut txn = pool.begin().await?;
-    set_power_shelf_controller_state(
-        txn.as_mut(),
-        &power_shelf_id,
-        PowerShelfControllerState::Ready,
-    )
-    .await?;
-    txn.commit().await?;
-
-    env.api
-        .decommission_power_shelf(Request::new(power_shelf_id))
-        .await?;
-
-    let mut controller = StateController::<PowerShelfStateControllerIO>::builder()
-        .iteration_config(IterationConfig {
-            iteration_time: Duration::from_millis(50),
-            processor_dispatch_interval: Duration::from_millis(10),
-            ..Default::default()
-        })
-        .database(pool.clone(), env.api.work_lock_manager_handle.clone())
-        .processor_id(uuid::Uuid::new_v4().to_string())
-        .services(services(&env, &pool).into())
-        .state_handler(Arc::new(PowerShelfStateHandler::default()))
-        .build_for_manual_iterations(CancellationToken::new())?;
-
-    controller.run_single_iteration().await;
-    assert_eq!(
-        load_power_shelf(&pool, power_shelf_id)
-            .await?
-            .controller_state
-            .value,
-        PowerShelfControllerState::Decommissioning {
-            decommissioning_state: PowerShelfDecommissioningState::SuppressingSiteExplorer,
-        }
-    );
-    controller.run_single_iteration().await;
-    assert!(
-        db::bmc_suppression::find(&pool, pmc_mac, BmcSuppressionSubsystem::SiteExplorer)
-            .await?
-            .is_some()
-    );
-
-    let mut txn = pool.begin().await?;
-    db::bmc_suppression::acknowledge_unacknowledged(
-        txn.as_mut(),
-        &[pmc_mac],
-        BmcSuppressionSubsystem::SiteExplorer,
-    )
-    .await?;
-    txn.commit().await?;
-    controller.run_single_iteration().await;
-    assert_eq!(
-        load_power_shelf(&pool, power_shelf_id)
-            .await?
-            .controller_state
-            .value,
-        PowerShelfControllerState::Decommissioning {
-            decommissioning_state: PowerShelfDecommissioningState::VerifyingDhcpRelease {
-                verifying_state: PowerShelfVerifyingDhcpReleaseState::FactoryResetBmc,
-            },
-        }
-    );
-
-    controller.run_single_iteration().await;
-    assert_eq!(
-        load_power_shelf(&pool, power_shelf_id)
-            .await?
-            .controller_state
-            .value,
-        PowerShelfControllerState::Decommissioning {
-            decommissioning_state: PowerShelfDecommissioningState::VerifyingDhcpRelease {
-                verifying_state: PowerShelfVerifyingDhcpReleaseState::SuppressingBmcDhcp,
-            },
-        }
-    );
-    assert!(
-        db::bmc_suppression::find(&pool, pmc_mac, BmcSuppressionSubsystem::Dhcp)
-            .await?
-            .is_none()
-    );
-
-    controller.run_single_iteration().await;
-    assert_eq!(
-        load_power_shelf(&pool, power_shelf_id)
-            .await?
-            .controller_state
-            .value,
-        PowerShelfControllerState::Decommissioning {
-            decommissioning_state: PowerShelfDecommissioningState::VerifyingDhcpRelease {
-                verifying_state:
-                    PowerShelfVerifyingDhcpReleaseState::WaitingForBmcDhcpAcknowledgement,
-            },
-        }
-    );
-    assert!(
-        db::bmc_suppression::find(&pool, pmc_mac, BmcSuppressionSubsystem::Dhcp)
-            .await?
-            .is_some()
-    );
-
-    let mut txn = pool.begin().await?;
-    db::bmc_suppression::acknowledge_unacknowledged(
-        txn.as_mut(),
-        &[pmc_mac],
-        BmcSuppressionSubsystem::Dhcp,
-    )
-    .await?;
-    txn.commit().await?;
-    controller.run_single_iteration().await;
-
-    let power_shelf = load_power_shelf(&pool, power_shelf_id).await?;
-    assert_eq!(
-        power_shelf.controller_state.value,
-        PowerShelfControllerState::Decommissioning {
-            decommissioning_state: PowerShelfDecommissioningState::Decommissioned,
-        }
-    );
-    assert!(!power_shelf.decommission_requested);
-
-    env.api
-        .delete_decommissioned_power_shelf(Request::new(power_shelf_id))
-        .await?;
-    let mut conn = pool.acquire().await?;
-    assert!(
-        db::power_shelf::find_by_id(conn.as_mut(), &power_shelf_id)
-            .await?
-            .is_none()
-    );
-    assert!(
-        db::machine_interface::find_ids_by_power_shelf_id(conn.as_mut(), &power_shelf_id)
-            .await?
-            .is_empty()
-    );
-    assert!(
-        db::bmc_suppression::find(&pool, pmc_mac, BmcSuppressionSubsystem::SiteExplorer)
-            .await?
-            .is_none()
-    );
-    assert!(
-        db::bmc_suppression::find(&pool, pmc_mac, BmcSuppressionSubsystem::Dhcp)
-            .await?
-            .is_none()
     );
     Ok(())
 }
