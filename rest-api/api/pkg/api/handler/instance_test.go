nskalskinv/infra-controller

feat(rest-api): filter instance placement by machine labels
Signed-off-by: Kun Zhao <kunzhao@nvidia.com>

rest-api/api/pkg/api/handler/instance_test.go
@@ -846,6 +846,12 @@ func TestCreateInstanceHandler_Handle(t *testing.T) {
 	assert.NotNil(t, alcbyid)
 	mcbyid := testInstanceBuildMachine(t, dbSession, ip.ID, st1.ID, cutil.GetPtr(false), nil)
 	assert.NotNil(t, mcbyid)
+	mcbyid.Labels = map[string]string{"failure-domain": "fd-a"}
+	_, err := cdbm.NewMachineDAO(dbSession).Update(ctx, nil, cdbm.MachineUpdateInput{
+		MachineID: mcbyid.ID,
+		Labels:    mcbyid.Labels,
+	})
+	assert.NoError(t, err)
 
 	// Add capability to machine
 	common.TestBuildMachineCapability(t, dbSession, &mcbyid.ID, nil, cdbm.MachineCapabilityTypeGPU, "NVIDIA GB200", nil, nil, cutil.GetPtr("NVIDIA"), cutil.GetPtr(4), cutil.GetPtr(cdbm.MachineCapabilityDeviceTypeNVLink), nil)
@@ -2183,6 +2189,34 @@ func TestCreateInstanceHandler_Handle(t *testing.T) {
 			wantErr: false,
 		},
 		{
+			name: "test Instance create API endpoint rejects specified Machine that does not match label filters",
+			fields: fields{
+				dbSession: dbSession,
+				tc:        tc,
+				cfg:       cfg,
+			},
+			args: args{
+				reqData: &model.APIInstanceCreateRequest{
+					Name:                "Test Instance with mismatched Machine labels",
+					TenantID:            tn1.ID.String(),
+					MachineID:           cutil.GetPtr(mcbyid.ID),
+					MachineLabelFilters: map[string]string{"failure-domain": "fd-b"},
+					VpcID:               vpc2.ID.String(),
+					UserData:            cutil.GetPtr(""),
+					IpxeScript:          cutil.GetPtr(common.DefaultIpxeScript),
+					Interfaces: []model.APIInterfaceCreateOrUpdateRequest{
+						{SubnetID: cutil.GetPtr(subnet2.ID.String())},
+					},
+					PhoneHomeEnabled: cutil.GetPtr(false),
+				},
+				reqOrg:      tnOrg,
+				reqUser:     tnu1,
+				respCode:    http.StatusBadRequest,
+				respMessage: "Machine specified in request does not match machineLabelFilters",
+			},
+			wantErr: false,
+		},
+		{
 			name: "test Instance create API endpoint success, specify a machine ID belonging to an instance type",
 			fields: fields{
 				dbSession: dbSession,
@@ -2191,12 +2225,13 @@ func TestCreateInstanceHandler_Handle(t *testing.T) {
 			},
 			args: args{
 				reqData: &model.APIInstanceCreateRequest{
-					Name:       "Test Instance with machine ID",
-					TenantID:   tn1.ID.String(),
-					MachineID:  cutil.GetPtr(mcbyid.ID),
-					VpcID:      vpc2.ID.String(),
-					UserData:   cutil.GetPtr(""),
-					IpxeScript: cutil.GetPtr(common.DefaultIpxeScript),
+					Name:                "Test Instance with machine ID",
+					TenantID:            tn1.ID.String(),
+					MachineID:           cutil.GetPtr(mcbyid.ID),
+					MachineLabelFilters: map[string]string{"failure-domain": "fd-a"},
+					VpcID:               vpc2.ID.String(),
+					UserData:            cutil.GetPtr(""),
+					IpxeScript:          cutil.GetPtr(common.DefaultIpxeScript),
 					Interfaces: []model.APIInterfaceCreateOrUpdateRequest{
 						{
 							SubnetID: cutil.GetPtr(subnet2.ID.String()),

rest-api/db/pkg/db/model/machine.go
@@ -287,6 +287,7 @@ type MachineFilterInput struct {
 	Statuses                  []string
 	SearchQuery               *string
 	MachineIDs                []string
+	MachineLabelFilters       map[string]string
 	IsMissingOnSite           *bool
 	ExcludeMetadata           bool // When true, excludes the metadata JSONB column from SELECT to improve performance on bulk queries
 }
@@ -684,11 +685,38 @@ func (msd MachineSQLDAO) setQueryWithFilter(filter MachineFilterInput, query *bu
 		query = query.Where("m.id IN (?)", bun.In(filter.MachineIDs))
 	}
 
+	// JSONB containment gives exact key/value AND semantics for the filter
+	// object and can use the machine labels GIN index.
+	if len(filter.MachineLabelFilters) > 0 {
+		labelFiltersJSON, err := json.Marshal(filter.MachineLabelFilters)
+		if err != nil {
+			return nil, fmt.Errorf("failed to encode Machine label filters: %w", err)
+		}
+		query = query.Where("m.labels @> ?::jsonb", string(labelFiltersJSON))
+		if machineDAOSpan != nil {
+			msd.tracerSpan.SetAttribute(machineDAOSpan, "machine_label_filters", string(labelFiltersJSON))
+		}
+	}
+
 	if filter.ExcludeMetadata {
 		query = query.ExcludeColumn("metadata")
 	}
 
 	return query, nil
+}
+
+// MatchesLabelFilters reports whether all requested label key/value pairs are
+// present on the Machine. An empty filter matches every Machine.
+func (m *Machine) MatchesLabelFilters(filters map[string]string) bool {
+	if m == nil {
+		return false
+	}
+	for key, value := range filters {
+		if actual, ok := m.Labels[key]; !ok || actual != value {
+			return false
+		}
+	}
+	return true
 }
 
 // GetAll returns all Machines based on the filter and paging

rest-api/sdk/standard/model_batch_instance_create_request.go
@@ -54,6 +54,8 @@ type BatchInstanceCreateRequest struct {
 	PhoneHomeEnabled *bool `json:"phoneHomeEnabled,omitempty"`
 	// Key-value objects to be applied to all instances (shared across all instances)
 	Labels map[string]string `json:"labels,omitempty"`
+	// Optional exact-match filters applied to Machine labels during placement. Property names are arbitrary Machine label keys rather than predefined filter fields. Every supplied key/value pair must match (AND semantics). An omitted or empty object does not restrict placement. The filters constrain placement only; they are not persisted on the created Instances.  Filtering occurs before topology optimization. When `topologyOptimized` is true, all selected Machines must both match these filters and belong to the same NVLink domain. If too few matching Machines are available, the request is rejected with 409.
+	MachineLabelFilters map[string]string `json:"machineLabelFilters,omitempty"`
 	// Interface configuration shared across all instances. At least one interface must be specified unless `autoNetwork` is true. Interfaces must all be Subnet-backed or all be VPC-backed; VPC-backed interfaces may use an explicit `vpcPrefixId` or ask the Controller to select a prefix using `vpcId` and `ipFamilies`. Each batch member is resolved independently and may use a different prefix. Only one network can be attached over a physical interface. Interface `ipAddress` is not supported for batch instance creation requests. Mutually exclusive with `autoNetwork`: when `autoNetwork` is true this list MUST be empty.
 	Interfaces []InterfaceCreateRequest `json:"interfaces,omitempty"`
 	// When true, asks NICo to auto-resolve each Instance's network interfaces from the host's underlay (HostInband) network segments. Intended for instances on zero-DPU hosts (or hosts with their DPU in NIC mode). When true: (1) the target VPC's `networkVirtualizationType` MUST be `FLAT`, (2) `interfaces` MUST be empty or omitted, and (3) `secondaryVpcIds` MUST be empty or omitted.
@@ -604,6 +606,38 @@ func (o *BatchInstanceCreateRequest) SetLabels(v map[string]string) {
 	o.Labels = v
 }
 
+// GetMachineLabelFilters returns the MachineLabelFilters field value if set, zero value otherwise.
+func (o *BatchInstanceCreateRequest) GetMachineLabelFilters() map[string]string {
+	if o == nil || IsNil(o.MachineLabelFilters) {
+		var ret map[string]string
+		return ret
+	}
+	return o.MachineLabelFilters
+}
+
+// GetMachineLabelFiltersOk returns a tuple with the MachineLabelFilters field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *BatchInstanceCreateRequest) GetMachineLabelFiltersOk() (map[string]string, bool) {
+	if o == nil || IsNil(o.MachineLabelFilters) {
+		return map[string]string{}, false
+	}
+	return o.MachineLabelFilters, true
+}
+
+// HasMachineLabelFilters returns a boolean if a field has been set.
+func (o *BatchInstanceCreateRequest) HasMachineLabelFilters() bool {
+	if o != nil && !IsNil(o.MachineLabelFilters) {
+		return true
+	}
+
+	return false
+}
+
+// SetMachineLabelFilters gets a reference to the given map[string]string and assigns it to the MachineLabelFilters field.
+func (o *BatchInstanceCreateRequest) SetMachineLabelFilters(v map[string]string) {
+	o.MachineLabelFilters = v
+}
+
 // GetInterfaces returns the Interfaces field value if set, zero value otherwise.
 func (o *BatchInstanceCreateRequest) GetInterfaces() []InterfaceCreateRequest {
 	if o == nil || IsNil(o.Interfaces) {
@@ -872,6 +906,9 @@ func (o BatchInstanceCreateRequest) ToMap() (map[string]interface{}, error) {
 	}
 	if !IsNil(o.Labels) {
 		toSerialize["labels"] = o.Labels
+	}
+	if !IsNil(o.MachineLabelFilters) {
+		toSerialize["machineLabelFilters"] = o.MachineLabelFilters
 	}
 	if !IsNil(o.Interfaces) {
 		toSerialize["interfaces"] = o.Interfaces

rest-api/sdk/standard/model_instance_create_request.go
@@ -54,6 +54,8 @@ type InstanceCreateRequest struct {
 	PhoneHomeEnabled *bool `json:"phoneHomeEnabled,omitempty"`
 	// User-defined key-value labels
 	Labels map[string]string `json:"labels,omitempty"`
+	// Optional exact-match filters applied to Machine labels during placement. Property names are arbitrary Machine label keys rather than predefined filter fields. Every supplied key/value pair must match (AND semantics). An omitted or empty object does not restrict placement.  When `instanceTypeId` is supplied, NICo selects from Ready, unassigned Machines of that Instance Type that match all filters. When `machineId` is supplied, the specified Machine must match all filters or the request is rejected with 400. The filters constrain placement only; they are not persisted on the created Instance.
+	MachineLabelFilters map[string]string `json:"machineLabelFilters,omitempty"`
 	// At least one interface must be specified unless `autoNetwork` is true. Interfaces must all be Subnet-backed or all be VPC-backed; VPC-backed interfaces may use an explicit `vpcPrefixId` or ask the Controller to select a prefix using `vpcId` and `ipFamilies`. Only one network can be attached over a physical interface. If only one Subnet is specified, it will be attached over a physical interface regardless of `isPhysical`. Mutually exclusive with `autoNetwork`: when `autoNetwork` is true this list MUST be empty.
 	Interfaces []InterfaceCreateRequest `json:"interfaces,omitempty"`
 	// When true, asks NICo to auto-resolve the Instance's network interfaces from the host's underlay (HostInband) network segments. Intended for instances on zero-DPU hosts (or hosts with their DPU in NIC mode). When true: (1) the target VPC's `networkVirtualizationType` MUST be `FLAT`, (2) `interfaces` MUST be empty or omitted, and (3) `secondaryVpcIds` MUST be empty or omitted. Resolved interfaces surface on the Instance's read response.
@@ -636,6 +638,38 @@ func (o *InstanceCreateRequest) SetLabels(v map[string]string) {
 	o.Labels = v
 }
 
+// GetMachineLabelFilters returns the MachineLabelFilters field value if set, zero value otherwise.
+func (o *InstanceCreateRequest) GetMachineLabelFilters() map[string]string {
+	if o == nil || IsNil(o.MachineLabelFilters) {
+		var ret map[string]string
+		return ret
+	}
+	return o.MachineLabelFilters
+}
+
+// GetMachineLabelFiltersOk returns a tuple with the MachineLabelFilters field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *InstanceCreateRequest) GetMachineLabelFiltersOk() (map[string]string, bool) {
+	if o == nil || IsNil(o.MachineLabelFilters) {
+		return map[string]string{}, false
+	}
+	return o.MachineLabelFilters, true
+}
+
+// HasMachineLabelFilters returns a boolean if a field has been set.
+func (o *InstanceCreateRequest) HasMachineLabelFilters() bool {
+	if o != nil && !IsNil(o.MachineLabelFilters) {
+		return true
+	}
+
+	return false
+}
+
+// SetMachineLabelFilters gets a reference to the given map[string]string and assigns it to the MachineLabelFilters field.
+func (o *InstanceCreateRequest) SetMachineLabelFilters(v map[string]string) {
+	o.MachineLabelFilters = v
+}
+
 // GetInterfaces returns the Interfaces field value if set, zero value otherwise.
 func (o *InstanceCreateRequest) GetInterfaces() []InterfaceCreateRequest {
 	if o == nil || IsNil(o.Interfaces) {
@@ -908,6 +942,9 @@ func (o InstanceCreateRequest) ToMap() (map[string]interface{}, error) {
 	}
 	if !IsNil(o.Labels) {
 		toSerialize["labels"] = o.Labels
+	}
+	if !IsNil(o.MachineLabelFilters) {
+		toSerialize["machineLabelFilters"] = o.MachineLabelFilters
 	}
 	if !IsNil(o.Interfaces) {
 		toSerialize["interfaces"] = o.Interfaces

NVIDIA/bare-metal-manager-core

rest-api/api/pkg/api/handler/instance_test.go
@@ -846,6 +846,12 @@ func TestCreateInstanceHandler_Handle(t *testing.T) {
 	assert.NotNil(t, alcbyid)
 	mcbyid := testInstanceBuildMachine(t, dbSession, ip.ID, st1.ID, cutil.GetPtr(false), nil)
 	assert.NotNil(t, mcbyid)
+	mcbyid.Labels = map[string]string{"failure-domain": "fd-a"}
+	_, err := cdbm.NewMachineDAO(dbSession).Update(ctx, nil, cdbm.MachineUpdateInput{
+		MachineID: mcbyid.ID,
+		Labels:    mcbyid.Labels,
+	})
+	assert.NoError(t, err)
 
 	// Add capability to machine
 	common.TestBuildMachineCapability(t, dbSession, &mcbyid.ID, nil, cdbm.MachineCapabilityTypeGPU, "NVIDIA GB200", nil, nil, cutil.GetPtr("NVIDIA"), cutil.GetPtr(4), cutil.GetPtr(cdbm.MachineCapabilityDeviceTypeNVLink), nil)
@@ -2183,6 +2189,62 @@ func TestCreateInstanceHandler_Handle(t *testing.T) {
 			wantErr: false,
 		},
 		{
+			name: "test Instance create API endpoint rejects Machine label selector when tenant is not authorized",
+			fields: fields{
+				dbSession: dbSession,
+				tc:        tc,
+				cfg:       cfg,
+			},
+			args: args{
+				reqData: &model.APIInstanceCreateRequest{
+					Name:                 "Test Instance with unauthorized Machine label selector",
+					TenantID:             tn2.ID.String(),
+					InstanceTypeID:       cutil.GetPtr(ist2.ID.String()),
+					MachineLabelSelector: map[string]string{"failure-domain": "fd-a"},
+					VpcID:                vpc3.ID.String(),
+					UserData:             cutil.GetPtr(""),
+					IpxeScript:           cutil.GetPtr(common.DefaultIpxeScript),
+					Interfaces: []model.APIInterfaceCreateOrUpdateRequest{
+						{SubnetID: cutil.GetPtr(subnet3.ID.String())},
+					},
+					PhoneHomeEnabled: cutil.GetPtr(false),
+				},
+				reqOrg:      tnOrg2,
+				reqUser:     tnu2,
+				respCode:    http.StatusForbidden,
+				respMessage: "Tenant does not have capability to create Instances using Machine label selector",
+			},
+			wantErr: false,
+		},
+		{
+			name: "test Instance create API endpoint rejects specified Machine that does not match label selector",
+			fields: fields{
+				dbSession: dbSession,
+				tc:        tc,
+				cfg:       cfg,
+			},
+			args: args{
+				reqData: &model.APIInstanceCreateRequest{
+					Name:                 "Test Instance with mismatched Machine labels",
+					TenantID:             tn1.ID.String(),
+					MachineID:            cutil.GetPtr(mcbyid.ID),
+					MachineLabelSelector: map[string]string{"failure-domain": "fd-b"},
+					VpcID:                vpc2.ID.String(),
+					UserData:             cutil.GetPtr(""),
+					IpxeScript:           cutil.GetPtr(common.DefaultIpxeScript),
+					Interfaces: []model.APIInterfaceCreateOrUpdateRequest{
+						{SubnetID: cutil.GetPtr(subnet2.ID.String())},
+					},
+					PhoneHomeEnabled: cutil.GetPtr(false),
+				},
+				reqOrg:      tnOrg,
+				reqUser:     tnu1,
+				respCode:    http.StatusBadRequest,
+				respMessage: "Machine specified in request does not match machineLabelSelector",
+			},
+			wantErr: false,
+		},
+		{
 			name: "test Instance create API endpoint success, specify a machine ID belonging to an instance type",
 			fields: fields{
 				dbSession: dbSession,
@@ -2191,12 +2253,13 @@ func TestCreateInstanceHandler_Handle(t *testing.T) {
 			},
 			args: args{
 				reqData: &model.APIInstanceCreateRequest{
-					Name:       "Test Instance with machine ID",
-					TenantID:   tn1.ID.String(),
-					MachineID:  cutil.GetPtr(mcbyid.ID),
-					VpcID:      vpc2.ID.String(),
-					UserData:   cutil.GetPtr(""),
-					IpxeScript: cutil.GetPtr(common.DefaultIpxeScript),
+					Name:                 "Test Instance with machine ID",
+					TenantID:             tn1.ID.String(),
+					MachineID:            cutil.GetPtr(mcbyid.ID),
+					MachineLabelSelector: map[string]string{"failure-domain": "fd-a"},
+					VpcID:                vpc2.ID.String(),
+					UserData:             cutil.GetPtr(""),
+					IpxeScript:           cutil.GetPtr(common.DefaultIpxeScript),
 					Interfaces: []model.APIInterfaceCreateOrUpdateRequest{
 						{
 							SubnetID: cutil.GetPtr(subnet2.ID.String()),

rest-api/db/pkg/db/model/machine.go
@@ -287,6 +287,7 @@ type MachineFilterInput struct {
 	Statuses                  []string
 	SearchQuery               *string
 	MachineIDs                []string
+	Labels                    map[string]string
 	IsMissingOnSite           *bool
 	ExcludeMetadata           bool // When true, excludes the metadata JSONB column from SELECT to improve performance on bulk queries
 }
@@ -684,11 +685,38 @@ func (msd MachineSQLDAO) setQueryWithFilter(filter MachineFilterInput, query *bu
 		query = query.Where("m.id IN (?)", bun.In(filter.MachineIDs))
 	}
 
+	// JSONB containment gives exact key/value AND semantics for the selector
+	// object and can use the machine labels GIN index.
+	if len(filter.Labels) > 0 {
+		labelsJSON, err := json.Marshal(filter.Labels)
+		if err != nil {
+			return nil, fmt.Errorf("failed to encode Machine label selector: %w", err)
+		}
+		query = query.Where("m.labels @> ?::jsonb", string(labelsJSON))
+		if machineDAOSpan != nil {
+			msd.tracerSpan.SetAttribute(machineDAOSpan, "machine_label_selector", string(labelsJSON))
+		}
+	}
+
 	if filter.ExcludeMetadata {
 		query = query.ExcludeColumn("metadata")
 	}
 
 	return query, nil
+}
+
+// MatchesLabelSelector reports whether all requested label key/value pairs are
+// present on the Machine. An empty selector matches every Machine.
+func (m *Machine) MatchesLabelSelector(selector map[string]string) bool {
+	if m == nil {
+		return false
+	}
+	for key, value := range selector {
+		if actual, ok := m.Labels[key]; !ok || actual != value {
+			return false
+		}
+	}
+	return true
 }
 
 // GetAll returns all Machines based on the filter and paging

rest-api/sdk/standard/model_batch_instance_create_request.go
@@ -34,6 +34,8 @@ type BatchInstanceCreateRequest struct {
 	TenantId string `json:"tenantId"`
 	// ID of the Instance Type to use for all Instances in the batch
 	InstanceTypeId string `json:"instanceTypeId"`
+	// Optional exact-match selector applied to Machine labels during placement. Property names are arbitrary Machine label keys rather than predefined selector fields. Every supplied key/value pair must match (AND semantics). An omitted or empty object does not restrict placement. The selector constrains placement only; it is not persisted on the created Instances.  A non-empty object requires the Tenant to have effective `targetedInstanceCreation` capability for the selected Site; otherwise the request is rejected with 403. Selection occurs before topology optimization. When `topologyOptimized` is true, all selected Machines must both match the selector and belong to the same NVLink domain. If too few matching Machines are available, the request is rejected with 409.
+	MachineLabelSelector map[string]string `json:"machineLabelSelector,omitempty"`
 	// ID of the VPC the Instances should belong to
 	VpcId string `json:"vpcId"`
 	// IDs of additional VPCs the Instances should attach to through non-primary interfaces. This field may only be specified when every entry in `interfaces` uses `vpcPrefixId` or `vpcId`. IDs must be unique, must be valid UUIDs, and must not include the primary `vpcId`.
@@ -235,6 +237,38 @@ func (o *BatchInstanceCreateRequest) GetInstanceTypeIdOk() (*string, bool) {
 // SetInstanceTypeId sets field value
 func (o *BatchInstanceCreateRequest) SetInstanceTypeId(v string) {
 	o.InstanceTypeId = v
+}
+
+// GetMachineLabelSelector returns the MachineLabelSelector field value if set, zero value otherwise.
+func (o *BatchInstanceCreateRequest) GetMachineLabelSelector() map[string]string {
+	if o == nil || IsNil(o.MachineLabelSelector) {
+		var ret map[string]string
+		return ret
+	}
+	return o.MachineLabelSelector
+}
+
+// GetMachineLabelSelectorOk returns a tuple with the MachineLabelSelector field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *BatchInstanceCreateRequest) GetMachineLabelSelectorOk() (map[string]string, bool) {
+	if o == nil || IsNil(o.MachineLabelSelector) {
+		return map[string]string{}, false
+	}
+	return o.MachineLabelSelector, true
+}
+
+// HasMachineLabelSelector returns a boolean if a field has been set.
+func (o *BatchInstanceCreateRequest) HasMachineLabelSelector() bool {
+	if o != nil && !IsNil(o.MachineLabelSelector) {
+		return true
+	}
+
+	return false
+}
+
+// SetMachineLabelSelector gets a reference to the given map[string]string and assigns it to the MachineLabelSelector field.
+func (o *BatchInstanceCreateRequest) SetMachineLabelSelector(v map[string]string) {
+	o.MachineLabelSelector = v
 }
 
 // GetVpcId returns the VpcId field value
@@ -845,6 +879,9 @@ func (o BatchInstanceCreateRequest) ToMap() (map[string]interface{}, error) {
 	}
 	toSerialize["tenantId"] = o.TenantId
 	toSerialize["instanceTypeId"] = o.InstanceTypeId
+	if !IsNil(o.MachineLabelSelector) {
+		toSerialize["machineLabelSelector"] = o.MachineLabelSelector
+	}
 	toSerialize["vpcId"] = o.VpcId
 	if !IsNil(o.SecondaryVpcIds) {
 		toSerialize["secondaryVpcIds"] = o.SecondaryVpcIds

rest-api/sdk/standard/model_instance_create_request.go
@@ -34,6 +34,8 @@ type InstanceCreateRequest struct {
 	InstanceTypeId NullableString `json:"instanceTypeId,omitempty"`
 	// ID of of specific Machine to use for Instance. Requires Targeted Instance Creation capability enabled for Tenant
 	MachineId NullableString `json:"machineId,omitempty"`
+	// Optional exact-match selector applied to Machine labels during placement. Property names are arbitrary Machine label keys rather than predefined selector fields. Every supplied key/value pair must match (AND semantics). An omitted or empty object does not restrict placement.  A non-empty object requires the Tenant to have effective `targetedInstanceCreation` capability for the selected Site; otherwise the request is rejected with 403. When `instanceTypeId` is supplied, NICo selects from Ready, unassigned Machines of that Instance Type that match the selector. When `machineId` is supplied, the specified Machine must match the selector or the request is rejected with 400. The selector constrains placement only; it is not persisted on the created Instance.
+	MachineLabelSelector map[string]string `json:"machineLabelSelector,omitempty"`
 	// ID of the VPC the Instance should belong to
 	VpcId string `json:"vpcId"`
 	// IDs of additional VPCs the Instance should attach to through non-primary interfaces. This field may only be specified when every entry in `interfaces` uses `vpcPrefixId` or `vpcId`. IDs must be unique, must be valid UUIDs, and must not include the primary `vpcId`.
@@ -267,6 +269,38 @@ func (o *InstanceCreateRequest) SetMachineIdNil() {
 // UnsetMachineId ensures that no value is present for MachineId, not even an explicit nil
 func (o *InstanceCreateRequest) UnsetMachineId() {
 	o.MachineId.Unset()
+}
+
+// GetMachineLabelSelector returns the MachineLabelSelector field value if set, zero value otherwise.
+func (o *InstanceCreateRequest) GetMachineLabelSelector() map[string]string {
+	if o == nil || IsNil(o.MachineLabelSelector) {
+		var ret map[string]string
+		return ret
+	}
+	return o.MachineLabelSelector
+}
+
+// GetMachineLabelSelectorOk returns a tuple with the MachineLabelSelector field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *InstanceCreateRequest) GetMachineLabelSelectorOk() (map[string]string, bool) {
+	if o == nil || IsNil(o.MachineLabelSelector) {
+		return map[string]string{}, false
+	}
+	return o.MachineLabelSelector, true
+}
+
+// HasMachineLabelSelector returns a boolean if a field has been set.
+func (o *InstanceCreateRequest) HasMachineLabelSelector() bool {
+	if o != nil && !IsNil(o.MachineLabelSelector) {
+		return true
+	}
+
+	return false
+}
+
+// SetMachineLabelSelector gets a reference to the given map[string]string and assigns it to the MachineLabelSelector field.
+func (o *InstanceCreateRequest) SetMachineLabelSelector(v map[string]string) {
+	o.MachineLabelSelector = v
 }
 
 // GetVpcId returns the VpcId field value
@@ -880,6 +914,9 @@ func (o InstanceCreateRequest) ToMap() (map[string]interface{}, error) {
 	}
 	if o.MachineId.IsSet() {
 		toSerialize["machineId"] = o.MachineId.Get()
+	}
+	if !IsNil(o.MachineLabelSelector) {
+		toSerialize["machineLabelSelector"] = o.MachineLabelSelector
 	}
 	toSerialize["vpcId"] = o.VpcId
 	if !IsNil(o.SecondaryVpcIds) {